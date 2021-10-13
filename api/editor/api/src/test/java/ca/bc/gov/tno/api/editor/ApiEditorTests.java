43718d48b2a05b41d6461d0a90c61c589963136d
fosol <jeremymfoster@hotmail.com>
2021-10-13 22:39:10 +0000

    Rename app

--- api/editor/api/src/test/java/ca/bc/gov/tno/api/editor/ApiEditorTests.java
+++ api/editor/api/src/test/java/ca/bc/gov/tno/api/editor/ApiEditorTests.java
@@ -1,10 +1,10 @@
-package ca.bc.gov.tno.api.editor.api;
+package ca.bc.gov.tno.api.editor;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiApplicationTests {
+class ApiEditorTests {
 
 	@Test
 	void contextLoads() {
