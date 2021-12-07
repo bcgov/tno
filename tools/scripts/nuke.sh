fc486a3fe9e8141fe776c38a667147be8ef0c408
fosol <jeremymfoster@hotmail.com>
2021-12-07 12:21:50 -0800

    Fix Environment Config

--- tools/scripts/nuke.sh
+++ tools/scripts/nuke.sh
@@ -7,10 +7,22 @@ echo "*****************************************************"
 echo ""
 
 echo 'This operation will delete your configuration files!'
+echo 'It will first make backup copies by renaming them (i.e. backup.env)'
 read -p 'Enter "y" to continue: ' varContinue
 
 if [ "$varContinue" == "y" ]; then
-  find . -type f -name '*.env' -exec rm {} +
+  echo "Deleting backup configurations"
+  find . -type f -name '*backup.env' -exec rm {} +
+  find . -type f -name 'flyway.backup.conf' -exec rm {} +
+  find . -type f -name 'maven-settings.backup.xml' -exec rm {} +
+
+  echo "Creating backup configurations"
+  find . -type f -name ".env" -exec sh -c 'cp {} `dirname {}`/` {} .env`backup.env' \;
+  find . -type f -name "flyway.conf" -exec sh -c 'cp {} `dirname {}`/`basename {} .conf`.backup.conf' \;
+  find . -type f -name "maven-settings.xml" -exec sh -c 'cp {} `dirname {}`/`basename {} .xml`.backup.xml' \;
+
+  echo "Deleting original configurations"
+  find . -type f -name '.env' -exec rm {} +
   find . -type f -name 'flyway.conf' -exec rm {} +
   find . -type f -name 'maven-settings.xml' -exec rm {} +
 fi
