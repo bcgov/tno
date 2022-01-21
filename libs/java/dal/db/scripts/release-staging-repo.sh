8282f0f0de3fff02a56e0649e02f21097bef0fb0
fosol <jeremymfoster@hotmail.com>
2022-01-21 22:31:44 +0000

    Fix maven gpg signing

--- libs/java/dal/db/scripts/release-staging-repo.sh
+++ libs/java/dal/db/scripts/release-staging-repo.sh
@@ -7,5 +7,10 @@ if [ ! -z "$varStagingRepoId" ]
 then
   cd dal-db
   export MAVEN_OPTS="--add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.text=ALL-UNNAMED --add-opens=java.desktop/java.awt.font=ALL-UNNAMED"
-  sudo env PATH="$PATH" mvn nexus-staging:release -P staging -DstagingRepositoryId="cabcgovtno-$varStagingRepoId" -s /home/vscode/.m2/settings.xml
+  
+  if [ "$1" = "sudo" ]; then
+    sudo env PATH="$PATH" mvn nexus-staging:release -P staging -DstagingRepositoryId="cabcgovtno-$varStagingRepoId" -s /home/vscode/.m2/settings.xml
+  else
+    mvn nexus-staging:release -P staging -DstagingRepositoryId="cabcgovtno-$varStagingRepoId" -s /home/vscode/.m2/settings.xml
+  fi
 fi
