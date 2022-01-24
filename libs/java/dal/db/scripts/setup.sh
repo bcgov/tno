69f4548d21859b4ec6e2d604c88d9bf7b3a2a0de
Jeremy Foster <jeremymfoster@hotmail.com>
2022-01-24 07:50:52 -0800

    Fix maven gpg signing (#35)

--- libs/java/dal/db/scripts/setup.sh
+++ libs/java/dal/db/scripts/setup.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-# This fixes a bug with GPG when signing the package.
-# export GPG_TTY=$(tty)
-# export MAVEN_OPTS="--add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.text=ALL-UNNAMED --add-opens=java.desktop/java.awt.font=ALL-UNNAMED"
-
 # Import the private.key that is in the root folder.
 # This is required to sign and verify packages.
-# sudo env "PATH=$PATH" gpg --import private.key
-sudo gpg --import private.key
+# sudo env "PATH=$PATH" gpg --import private.key]
+FILE=private.key
+if [ -f "$FILE" ]; then
+  if [ "$1" = "sudo" ]; then
+    sudo gpg --import "$FILE"
+    sudo cp .devcontainer/gpg.conf ~/.gnupg/gpg.conf
+    sudo cp .devcontainer/gpg-agent.conf ~/.gnupg/gpg-agent.conf
+  else
+    gpg --import "$FILE"
+    cp .devcontainer/gpg.conf ~/.gnupg/gpg.conf
+    cp .devcontainer/gpg-agent.conf ~/.gnupg/gpg-agent.conf
+  fi
+fi
+
+echo RELOADAGENT | gpg-connect-agent
+
+# sudo echo 'default-key:0:"xxxxxx' | gpgconf —change-options gpg
