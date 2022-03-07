b3999581bb21b5a23ee5d98b0accc2b40527878f
fosol <jeremymfoster@hotmail.com>
2022-03-07 12:04:15 -0800

    Fix content list view filter

---
+++ app/editor/src/features/content/list-view/constants/defaultFilter.ts
@@ -0,0 +1,16 @@
+import { IContentListFilter } from '../interfaces';
+import { defaultPage } from './defaultPage';
+
+export const defaultFilter: IContentListFilter = {
+  pageIndex: defaultPage.pageIndex,
+  pageSize: defaultPage.pageIndex,
+  mediaTypeId: 0,
+  contentTypeId: 0,
+  ownerId: 0,
+  userId: 0,
+  timeFrame: 0,
+  included: '',
+  onTicker: '',
+  commentary: '',
+  topStory: '',
+};
--- app/editor/src/store/slices/content/useContentStore.ts
+++ app/editor/src/store/slices/content/useContentStore.ts
@@ -5,9 +5,14 @@ import {
 } from 'features/content/list-view/interfaces';
 import React from 'react';
 import { useAppDispatch, useAppSelector } from 'store';
+import useDeepCompareEffect from 'tno-core/dist/hooks/useDeepCompareEffect';
 
 import { storeFilter, storeFilterAdvanced, storeSortBy } from '.';
 import { IContentState } from './interfaces';
+
+export interface IContentProps {
+  filter: IContentListFilter;
+}
 
 export interface IContentStore {
   storeFilter: (filter: IContentListFilter) => void;
@@ -15,7 +20,7 @@ export interface IContentStore {
   storeSortBy: (sortBy: ISortBy[]) => void;
 }
 
-export const useContentStore = (): [IContentState, IContentStore] => {
+export const useContentStore = (props?: IContentProps): [IContentState, IContentStore] => {
   const dispatch = useAppDispatch();
   const state = useAppSelector((store) => store.content);
 
@@ -33,6 +38,12 @@ export const useContentStore = (): [IContentState, IContentStore] => {
     }),
     [dispatch],
   );
+
+  useDeepCompareEffect(() => {
+    if (props?.filter) {
+      controller.storeFilter(props?.filter);
+    }
+  }, [controller, props?.filter]);
 
   return [state, controller];
 };
