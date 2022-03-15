40a9a01d9375d34b9feba45dd78956ccf3a8db34
jdtoombs <jdtoombs@gmail.com>
2022-03-15 09:58:50 -0700

    Modal tweaks

--- app/editor/src/components/modal/Modal.tsx
+++ app/editor/src/components/modal/Modal.tsx
@@ -8,8 +8,8 @@ import * as styled from './styled';
 export interface IModalProps {
   /** boolean value used to determine whether to show the modal or not */
   isShowing: boolean;
-  /** boolean value used to determine whether to hide the modal or not */
-  hide: () => void;
+  /** function used to toggle the modal */
+  setIsShowing: (value: boolean) => void;
   /** the text to show along with the cancel button to close the modal */
   cancelText?: string;
   /** the text to show along with the confirm button on the modal */
@@ -18,49 +18,47 @@ export interface IModalProps {
   onConfirm?: () => void;
   /** the text for the header */
   headerText?: string;
-  /** the text for the body of the modal */
-  bodyText?: string | React.ReactFragment;
+  /** the text / fragment for the body of the modal */
+  body?: string | React.ReactFragment;
 }
 
-/** Modal used to display informative and confirmation messages through the application.
- * Portal is used to allow the modal to be appended outside of inner elements */
-const Modal: React.FC<IModalProps> = ({
+/**
+ *  Modal used to display informative and confirmation messages through the application.
+ *  Portal is used to allow the modal to be appended outside of inner elements.
+ **/
+export const Modal: React.FC<IModalProps> = ({
+  setIsShowing,
   isShowing,
-  hide,
   cancelText,
   confirmText,
   onConfirm,
   headerText,
-  bodyText,
+  body,
 }) =>
   isShowing
     ? ReactDOM.createPortal(
-        <>
-          <styled.Modal>
-            <div className="modal-overlay">
-              <div className="modal-wrapper" aria-modal aria-hidden tabIndex={-1} role="dialog">
-                <div className="modal">
-                  <Row className="modal-header">
-                    <h1>{headerText}</h1>
-                  </Row>
-                  <Row>
-                    <p>{bodyText}</p>
-                  </Row>
-                  <Row className="button-row">
-                    <Button variant={ButtonVariant.action} onClick={onConfirm}>
-                      {confirmText ?? 'Continue'}
-                    </Button>
-                    <Button variant={ButtonVariant.danger} onClick={hide}>
-                      {cancelText ?? 'Cancel'}
-                    </Button>
-                  </Row>
-                </div>
+        <styled.Modal>
+          <div className="modal-overlay">
+            <div className="modal-wrapper" aria-modal aria-hidden tabIndex={-1} role="dialog">
+              <div className="modal">
+                <Row className="modal-header">
+                  <h1>{headerText}</h1>
+                </Row>
+                <Row>
+                  <p>{body}</p>
+                </Row>
+                <Row className="button-row">
+                  <Button variant={ButtonVariant.action} onClick={onConfirm}>
+                    {confirmText ?? 'Continue'}
+                  </Button>
+                  <Button variant={ButtonVariant.danger} onClick={() => setIsShowing(!isShowing)}>
+                    {cancelText ?? 'Cancel'}
+                  </Button>
+                </Row>
               </div>
             </div>
-          </styled.Modal>
-        </>,
+          </div>
+        </styled.Modal>,
         document.body,
       )
     : null;
-
-export default Modal;
