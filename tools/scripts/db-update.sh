fffde36d54a342fcbf1c645dbea975c1cb3b018c
Stuart Morse <stuart.morse@quartech.com>
2022-03-18 15:40:39 -0700

    Second round of changes for PR #73

--- tools/scripts/db-update.sh
+++ tools/scripts/db-update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd libs/java/dal/db
-docker build tno:db-migration dal-db-migration --no-cache --force-rm
+docker build -t tno:db-migration dal-db-migration --no-cache --force-rm
 docker run -it --env-file=.env --name tno-db-migration tno:db-migration
 docker rm tno-db-migration
