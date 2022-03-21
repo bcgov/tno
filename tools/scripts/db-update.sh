71c02ec31769a8435463863628099382ba855e7e
Stuart Morse <stuart.morse@quartech.com>
2022-03-21 08:18:20 -0700

    Removing t option from docker command

--- tools/scripts/db-update.sh
+++ tools/scripts/db-update.sh
@@ -2,5 +2,5 @@
 
 cd libs/java/dal/db
 docker build -t tno:db-migration dal-db-migration --no-cache --force-rm
-docker run -it --env-file=.env --name tno-db-migration tno:db-migration
+docker run -i --env-file=.env --name tno-db-migration tno:db-migration
 docker rm tno-db-migration
