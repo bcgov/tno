ea3e5bf2e1e61a854991c936f503a0306ca01a16
Jonny Toombs <jdtoombs@gmail.com>
2022-08-05 07:30:34 -0700

    TNO-321: Consitent Form Page (#212)

--- app/editor/src/components/form/formpage/styled/FormPage.tsx
+++ app/editor/src/components/form/formpage/styled/FormPage.tsx
@@ -5,6 +5,7 @@ import { IFormPageProps } from '../FormPage';
 export const FormPage = styled.div<IFormPageProps>`
   background-color: white;
   min-height: 100%;
+  min-width: 1200px;
   padding: 0.5em 2em 0 2em;
   margin: 0px auto;
 `;
