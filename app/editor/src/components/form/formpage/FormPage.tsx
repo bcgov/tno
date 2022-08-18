934d561eebf09025f8c45abf5242ee16fb14a300
jdtoombs <jdtoombs@gmail.com>
2022-08-17 23:35:01 -0700

    TNO-430: Hidden expand button on combined view

--- app/editor/src/components/form/formpage/FormPage.tsx
+++ app/editor/src/components/form/formpage/FormPage.tsx
@@ -3,6 +3,9 @@ import { InputHTMLAttributes } from 'react';
 import * as styled from './styled';
 
 export interface IFormPageProps extends InputHTMLAttributes<HTMLDivElement> {
+  /** bypass the default min width */
+  bypassMinWidth?: boolean;
+  /** include contents of form page */
   children: React.ReactNode;
 }
 /** Provides a consistent white background "page" used throughout the application */
--- app/editor/src/components/form/formpage/styled/FormPage.tsx
+++ app/editor/src/components/form/formpage/styled/FormPage.tsx
@@ -5,7 +5,7 @@ import { IFormPageProps } from '../FormPage';
 export const FormPage = styled.div<IFormPageProps>`
   background-color: white;
   min-height: 100%;
-  min-width: 1200px;
+  min-width: ${(props) => !props.bypassMinWidth && '1200px'};
   padding: 0.5em 2em 0 2em;
   margin: 0px auto;
 `;
