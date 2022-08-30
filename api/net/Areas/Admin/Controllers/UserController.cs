0863e9ff03a2c542a38305c5b97c9d65b5a7d2ef
Cello Liu <liu.cello@gmail.com>
2022-08-30 13:39:09 -0700

    Fixed TNO-444

--- api/net/Areas/Admin/Controllers/UserController.cs
+++ api/net/Areas/Admin/Controllers/UserController.cs
@@ -60,7 +60,8 @@ public class UserController : ControllerBase
         var uri = new Uri(this.Request.GetDisplayUrl());
         var query = Microsoft.AspNetCore.WebUtilities.QueryHelpers.ParseQuery(uri.Query);
         var result = _userService.Find(new UserFilter(query));
-        var page = new Paged<UserModel>(result.Items.Select(ds => new UserModel(ds)), result.Page, result.Quantity, result.Total);
+        var items = result.Items.Where(u => !u.IsSystemAccount).Select(ds => new UserModel(ds));
+        var page = new Paged<UserModel>(items, result.Page, result.Quantity, items.Count());
         return new JsonResult(page);
     }
 
