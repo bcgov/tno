9843e908f0efb365d67473164c713569500f6448
Jonny Toombs <jdtoombs@gmail.com>
2022-08-31 10:16:03 -0700

    TNO-377: CHES Integration (#233)
    * TNO-377: CHES Integration
    
    * Tweak
    
    * Use IOpenIdConnectRequestClient
    
    * Addressing comments
    
    * Remove unused var

---
+++ libs/net/ches/Models/IEmailContext.cs
@@ -0,0 +1,21 @@
+using TNO.Core.Converters;
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace TNO.Ches.Models
+{
+    public interface IEmailContext
+    {
+        IEnumerable<string> Bcc { get; set; }
+        IEnumerable<string> Cc { get; set; }
+        object Context { get; set; }
+
+        [JsonConverter(typeof(MicrosecondEpochJsonConverter))]
+        [JsonPropertyName("delayTS")]
+        DateTime SendOn { get; set; }
+
+        string Tag { get; set; }
+        IEnumerable<string> To { get; set; }
+    }
+}
