952d6ffb58254d1487d936718954717c504b8e07
Cello Liu <liu.cello@gmail.com>
2022-09-07 15:05:19 -0700

    Fixed TNO-586

--- libs/net/dal/Migrations/Initial/Up/PostUp/02-DataSourceSchedule.sql
+++ libs/net/dal/Migrations/Initial/Up/PostUp/02-DataSourceSchedule.sql
@@ -3,6 +3,7 @@ DECLARE DEFAULT_USER_ID UUID := '00000000-0000-0000-0000-000000000000';
 DECLARE CBC_CAPTURE_ID integer := (SELECT id FROM public.data_source WHERE public.data_source.code = 'CBCV-CAPTURE');
 DECLARE CBC_CLIP_ID integer := (SELECT id FROM public.data_source WHERE public.data_source.code = 'CBCV');
 DECLARE CASTANET_ID integer := (SELECT id FROM public.data_source WHERE public.data_source.code = 'CASTANET');
+DECLARE CPNEWS_ID integer := (SELECT id FROM public.data_source WHERE public.data_source.code = 'CPNEWS');
 BEGIN
 
 IF (CBC_CAPTURE_ID IS NOT NULL) THEN
@@ -66,6 +67,30 @@ IF (CASTANET_ID IS NOT NULL) THEN
     , "version"
   ) VALUES (
     CASTANET_ID  -- data_source_id
+    , 3  -- schedule_id
+    , DEFAULT_USER_ID
+    , ''
+    , CURRENT_TIMESTAMP
+    , DEFAULT_USER_ID
+    , ''
+    , CURRENT_TIMESTAMP
+    , 0
+  );
+END IF;
+
+IF (CPNEWS_ID IS NOT NULL) THEN
+  INSERT INTO public.data_source_schedule (
+    "data_source_id"
+    , "schedule_id"
+    , "created_by_id"
+    , "created_by"
+    , "created_on"
+    , "updated_by_id"
+    , "updated_by"
+    , "updated_on"
+    , "version"
+  ) VALUES (
+    CPNEWS_ID  -- data_source_id
     , 3  -- schedule_id
     , DEFAULT_USER_ID
     , ''
