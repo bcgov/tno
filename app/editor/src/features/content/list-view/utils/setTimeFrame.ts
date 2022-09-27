a31f35e5af92b233741d0f3730d18c949252656d
Jeremy Foster <jeremy.foster@fosol.ca>
2022-09-27 16:30:56 +0000

    Update dependencies Update tno-core v0.0.173 Fix filter bug

--- app/editor/src/features/content/list-view/utils/setTimeFrame.ts
+++ app/editor/src/features/content/list-view/utils/setTimeFrame.ts
@@ -4,4 +4,5 @@ export const setTimeFrame = (timeFrame: number) => {
   if (timeFrame === 0) return moment().startOf('day').toDate();
   else if (timeFrame === 1) return moment().add(-24, 'hours').toDate();
   else if (timeFrame === 2) return moment().add(-48, 'hours').toDate();
+  return undefined;
 };
