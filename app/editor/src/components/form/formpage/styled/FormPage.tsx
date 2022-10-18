3d18d520189eb0e1dcf5047febb83e2500b3d9e7
jdtoombs <jdtoombs@gmail.com>
2022-10-18 15:37:19 -0700

    Form page expand to children

--- app/editor/src/components/form/formpage/styled/FormPage.tsx
+++ app/editor/src/components/form/formpage/styled/FormPage.tsx
@@ -4,9 +4,11 @@ import { IFormPageProps } from '../FormPage';
 
 export const FormPage = styled.div<IFormPageProps>`
   background-color: white;
-  min-height: 100%;
+  min-height: fit-content;
   ${(props) => (props.minWidth !== '' ? `min-width: ${props.minWidth ?? '1200px'}` : '')};
   ${(props) => (props.maxWidth !== '' ? `max-width: ${props.maxWidth ?? '1200px'}` : '')};
   padding: 0.5em 2em 0 2em;
   margin: 0px auto;
+  overflow-y: overlay;
+  overflow-x: hidden;
 `;
