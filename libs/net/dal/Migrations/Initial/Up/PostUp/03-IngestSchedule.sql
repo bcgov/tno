00452bd40298c11445e42caf0eb67d2dbcf491b0
AlessiaYChen <42006584+AlessiaYChen@users.noreply.github.com>
2022-10-24 14:53:57 -0700

    add front page ingest (#358)
    Co-authored-by: Alessia Yan Chen <ayanchen@Alessias-MacBook-Pro.local>

--- libs/net/dal/Migrations/Initial/Up/PostUp/03-IngestSchedule.sql
+++ libs/net/dal/Migrations/Initial/Up/PostUp/03-IngestSchedule.sql
@@ -101,6 +101,42 @@ INSERT INTO public.ingest_schedule (
   , DEFAULT_USER_ID
   , ''
   , CURRENT_TIMESTAMP
+), (
+  (SELECT id FROM public.ingest WHERE name = 'The Province - Front Pages')  -- ingest_id
+  , (SELECT id FROM public.schedule WHERE name = 'PROVINCE')  -- schedule_id
+  , DEFAULT_USER_ID
+  , ''
+  , CURRENT_TIMESTAMP
+  , DEFAULT_USER_ID
+  , ''
+  , CURRENT_TIMESTAMP
+), (
+  (SELECT id FROM public.ingest WHERE name = 'Times Colonist Victoria - Front Pages')  -- ingest_id
+  , (SELECT id FROM public.schedule WHERE name = 'TC')  -- schedule_id
+  , DEFAULT_USER_ID
+  , ''
+  , CURRENT_TIMESTAMP
+  , DEFAULT_USER_ID
+  , ''
+  , CURRENT_TIMESTAMP
+), (
+  (SELECT id FROM public.ingest WHERE name = 'Vancouver Sun - Front Pages')  -- ingest_id
+  , (SELECT id FROM public.schedule WHERE name = 'SUN')  -- schedule_id
+  , DEFAULT_USER_ID
+  , ''
+  , CURRENT_TIMESTAMP
+  , DEFAULT_USER_ID
+  , ''
+  , CURRENT_TIMESTAMP
+), (
+  (SELECT id FROM public.ingest WHERE name = 'National Post - Front Pages')  -- ingest_id
+  , (SELECT id FROM public.schedule WHERE name = 'POST')  -- schedule_id
+  , DEFAULT_USER_ID
+  , ''
+  , CURRENT_TIMESTAMP
+  , DEFAULT_USER_ID
+  , ''
+  , CURRENT_TIMESTAMP
 );
 
 END $$;
