7141c5564c5720783f1696fb474c410ba74033a7
Cello Liu <liu.cello@gmail.com>
2022-11-14 09:52:27 -0800

    TNO-697 updates just for testing on dev only

--- app/editor/src/hooks/api-editor/editor/useApiHubConnection.ts
+++ app/editor/src/hooks/api-editor/editor/useApiHubConnection.ts
@@ -4,8 +4,10 @@ import React from 'react';
 export const useApiHubConnection = () => {
   const controller = React.useRef({
     getConnection: () => {
+      // TODO: here just for testing only for dev environment, and will be updated later on
+      const url = 'https://tno-dev.apps.silver.devops.gov.bc.ca/api/workOrderHub';
       const connection = new HubConnectionBuilder()
-        .withUrl(process.env.REACT_APP_API_URL + '/workOrderHub', { withCredentials: false })
+        .withUrl(url, { withCredentials: false }) // process.env.REACT_APP_API_URL + '/workOrderHub'
         .withAutomaticReconnect()
         .build();
       return connection;
