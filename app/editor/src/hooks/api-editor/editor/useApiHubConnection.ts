6ab702c656f02e64bcf3c8e85e9e2856a81e4b09
CelloLiu <55408610+CelloLiu@users.noreply.github.com>
2022-11-16 10:22:20 -0800

    TNO-697 updates just for testing on dev only (#395)
    * TNO-697 updates just for testing on dev only
    
    * TNO-697 updates for testing on dev
    
    * TNO-697 updates for testing on dev #2

--- app/editor/src/hooks/api-editor/editor/useApiHubConnection.ts
+++ app/editor/src/hooks/api-editor/editor/useApiHubConnection.ts
@@ -4,8 +4,10 @@ import React from 'react';
 export const useApiHubConnection = () => {
   const controller = React.useRef({
     getConnection: () => {
+      const url = process.env.REACT_APP_API_URL + '/api/workOrderHub';
+      console.log(`REACT_APP_API_URL: ${process.env.REACT_APP_API_URL}`);
       const connection = new HubConnectionBuilder()
-        .withUrl(process.env.REACT_APP_API_URL + '/workOrderHub', { withCredentials: false })
+        .withUrl(url, { withCredentials: false })
         .withAutomaticReconnect()
         .build();
       return connection;
