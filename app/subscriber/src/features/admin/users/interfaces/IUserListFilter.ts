be79750d8fc44dea34b75f07156a4eda1da3ee17
Jeremy Foster <jeremy.foster@fosol.ca>
2023-01-11 16:16:09 -0800

    TNO-859 Fix templates (#473)
    Add missing subscriber interfaces

---
+++ app/subscriber/src/features/admin/users/interfaces/IUserListFilter.ts
@@ -0,0 +1,18 @@
+import { ISortBy } from 'features/content/list-view/interfaces';
+import { UserStatusName } from 'hooks';
+
+export interface IUserListFilter {
+  username?: string;
+  email?: string;
+  name?: string;
+  firstName?: string;
+  lastName?: string;
+  isEnabled?: boolean;
+  isSystemAccount?: boolean;
+  status?: UserStatusName;
+  sort: ISortBy[];
+  roleName?: string;
+  keyword?: string;
+  pageIndex: number;
+  pageSize: number;
+}
