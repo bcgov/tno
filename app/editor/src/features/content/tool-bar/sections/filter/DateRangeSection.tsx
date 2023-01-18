55cde1aed6e6b0780f55db6a3648b0269620ae66
jdtoombs <jdtoombs@gmail.com>
2023-01-18 14:03:09 -0800

    TNO-888, 887: Date range trigger and clear

---
+++ app/editor/src/features/content/tool-bar/sections/filter/DateRangeSection.tsx
@@ -0,0 +1,68 @@
+import {
+  IContentListAdvancedFilter,
+  IContentListFilter,
+} from 'features/content/list-view/interfaces';
+import React from 'react';
+import { FaCalendarAlt } from 'react-icons/fa';
+import { useContent } from 'store/hooks';
+import { SelectDate } from 'tno-core';
+
+import * as styled from './styled';
+
+export interface IDateRangeSectionProps {
+  onAdvancedFilterChange: (filter: IContentListAdvancedFilter) => void;
+  onSearch: (filter: IContentListFilter & IContentListAdvancedFilter) => void;
+}
+export const DateRangeSection: React.FC<IDateRangeSectionProps> = ({
+  onAdvancedFilterChange,
+  onSearch,
+}) => {
+  const [{ filter, filterAdvanced }] = useContent();
+  /** retrigger fetch on change of date or clear*/
+  React.useEffect(() => {
+    onSearch({ ...filter, pageIndex: 0, ...filterAdvanced });
+  }, [filter, filterAdvanced, onSearch]);
+
+  return (
+    <styled.DateRangeSection>
+      <FaCalendarAlt className="action-icon calendar" />
+      <SelectDate
+        name="startDate"
+        placeholderText="mm/dd/yyyy"
+        selected={!!filterAdvanced.startDate ? new Date(filterAdvanced.startDate) : undefined}
+        width="8em"
+        onChange={(date) =>
+          onAdvancedFilterChange({
+            ...filterAdvanced,
+            startDate: !!date ? date.toString() : undefined,
+          })
+        }
+      />
+      <SelectDate
+        name="endDate"
+        placeholderText="mm/dd/yyyy"
+        selected={!!filterAdvanced.endDate ? new Date(filterAdvanced.endDate) : undefined}
+        width="8em"
+        onChange={(date) => {
+          date?.setHours(23, 59, 59);
+          onAdvancedFilterChange({
+            ...filterAdvanced,
+            endDate: !!date ? date.toString() : undefined,
+          });
+        }}
+      />
+      <span
+        className="clear"
+        onClick={() => {
+          onAdvancedFilterChange({
+            ...filterAdvanced,
+            startDate: undefined,
+            endDate: undefined,
+          });
+        }}
+      >
+        X
+      </span>
+    </styled.DateRangeSection>
+  );
+};
--- app/editor/src/features/content/tool-bar/sections/filter/index.ts
+++ app/editor/src/features/content/tool-bar/sections/filter/index.ts
@@ -1,5 +1,6 @@
 export * from './AdvancedSearchSection';
 export * from './CreateNewSection';
+export * from './DateRangeSection';
 export * from './FilterContentSection';
 export * from './InputOption';
 export * from './ShowOnlySection';
