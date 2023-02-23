50a6a7168d33fa09a4f4df9748360320601150c3
jdtoombs <jdtoombs@gmail.com>
2023-02-23 10:45:32 -0800

    Change message

--- app/editor/src/features/content/validation/ContentFormSchema.ts
+++ app/editor/src/features/content/validation/ContentFormSchema.ts
@@ -22,7 +22,7 @@ export const ContentFormSchema = object().shape({
   headline: string().required('Headline is a required field.'),
   tonePools: number().when('contentType', {
     is: (value: ContentTypeName) => value !== ContentTypeName.Image,
-    then: number().required('Tone is a required field.').typeError('Tone is a required field.'),
+    then: number().required('Tone is a required field.').typeError('A numeric tone value is required.'),
   }),
   // TODO: validation for print content.
 });
