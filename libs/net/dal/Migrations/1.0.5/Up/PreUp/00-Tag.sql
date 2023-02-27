ee770a30692a629d7f38fe26baad06f76a8a1e0f
jdtoombs <jdtoombs@gmail.com>
2023-02-27 14:40:21 -0800

    Deal with foreign keys first

--- libs/net/dal/Migrations/1.0.5/Up/PreUp/00-Tag.sql
+++ libs/net/dal/Migrations/1.0.5/Up/PreUp/00-Tag.sql
@@ -12,13 +12,7 @@ CREATE SEQUENCE seq START 1;
 UPDATE _tag
 SET new_id = nextval('seq');
 
--- set id of publig.tag to match id of _tag table
-UPDATE public.tag
-SET "id" = _tag.new_id
-FROM _tag
-WHERE _tag.name = public.tag.name;
-
--- update content_tag table to match new id
+-- adjust foreign keys to correct values
 UPDATE public.content_tag
 SET "tag_id" = _tag.new_id
 FROM _tag
@@ -27,11 +21,15 @@ WHERE _tag.id = public.content_tag.tag_id;
 -- drop foreign key constraint on content_tag table
 ALTER TABLE public.content_tag DROP CONSTRAINT "FK_content_tag_tag_tag_id";
 
-
 ALTER TABLE public.content_tag ALTER COLUMN "tag_id" SET DATA TYPE integer USING "tag_id"::integer;
-ALTER TABLE public.tag ALTER COLUMN "id" SET DATA TYPE integer USING "id"::integer;
 
--- add foreign key constraint on content_tag table
+-- change primary key to correct values
+UPDATE public.tag
+SET "id" = _tag.new_id
+FROM _tag
+WHERE _tag.name = public.tag.name;
+
+ALTER TABLE public.tag ALTER COLUMN "id" SET DATA TYPE integer USING "id"::integer;
 ALTER TABLE public.content_tag ADD CONSTRAINT "FK_content_tag_tag_tag_id" FOREIGN KEY ("tag_id") REFERENCES public.tag ("id") ON DELETE CASCADE;
 
 END $$;
