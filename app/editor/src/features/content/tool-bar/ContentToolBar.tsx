36f609ca20cb4884c7466bedc15812d36e636e14
CelloLiu <55408610+CelloLiu@users.noreply.github.com>
2023-04-06 18:37:52 -0700

    TNO-394 (fix an url related issue) (#658)

--- app/editor/src/features/content/tool-bar/ContentToolBar.tsx
+++ app/editor/src/features/content/tool-bar/ContentToolBar.tsx
@@ -20,21 +20,11 @@ export interface IContentToolBarProps {
  * @returns A content filter toolbar
  */
 export const ContentToolBar: React.FC<IContentToolBarProps> = ({ onSearch }) => {
-  const [{ filter, filterAdvanced }, { storeFilter, storeFilterAdvanced }] = useContent();
+  const [{ filterAdvanced }, { storeFilterAdvanced }] = useContent();
   const search = fromQueryString(window.location.search);
-
-  // extract every value from window.location.search that is productIds
-  // and put into an array
-  const extractProductIds = (search: string) => {
-    let params = new URLSearchParams(search);
-    const productIds = params.getAll('productIds');
-    const productIdsIntArray = productIds.map((item) => parseInt(item));
-    storeFilter({ ...filter, productIds: productIdsIntArray });
-  };
 
   React.useEffect(() => {
     if (!!window.location.search) {
-      if (!!search.productIds) extractProductIds(search.productIds);
       Object.keys(search).forEach(function (key) {
         if (key in filterAdvanced && search[key] !== undefined) {
           storeFilterAdvanced({ ...filterAdvanced, [key]: search[key] });
