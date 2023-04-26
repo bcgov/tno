17c7c1b693ebf20023cb98a32737e77564050aed
Jeremy Foster <jeremy.foster@fosol.ca>
2023-04-26 14:07:41 -0700

    TNO-1180 Fix topic score rules (#689)

--- app/editor/src/features/admin/topic-score-rules/styled/TopicScoreRuleList.tsx
+++ app/editor/src/features/admin/topic-score-rules/styled/TopicScoreRuleList.tsx
@@ -23,84 +23,6 @@ export const TopicScoreRuleList = styled.div`
     background-color: #f5f5f5;
   }
 
-  .row-header {
-    font-weight: bold;
-    padding: 0.25em 0.5em 0.25em 0.5em;
-    border-top: solid 2px black;
-    border-bottom: solid 2px black;
-
-    .f2 {
-      flex: 2 2 0;
-    }
-    .f3 {
-      flex: 3 2 0;
-    }
-    & > div {
-      flex: 1 1 0;
-    }
-  }
-
-  .row {
-    padding: 0.5em 0.5em 0 0.5em;
-
-    .f2 {
-      flex: 2 2 0;
-    }
-    .f3 {
-      flex: 3 2 0;
-    }
-    & > div {
-      flex: 1 1 0;
-    }
-
-    &:nth-child(4n) {
-      background-color: #f5f5f5;
-    }
-
-    textarea {
-      height: 39.6px;
-    }
-
-    &.adding {
-      background-color: ${(props) => props.theme.css.primaryLightColor};
-      border-radius: 0.5em;
-    }
-
-    .actions {
-      flex-wrap: nowrap;
-    }
-
-    button.move {
-      height: 40%;
-
-      padding: 0;
-      margin-right: 0.5em;
-
-      svg {
-        height: 100%;
-      }
-    }
-  }
-
-  .add-row {
-    background-color: ${(props) => props.theme.css.borderColor};
-    height: 5px;
-    align-content: center;
-    justify-content: center;
-    z-index: 2;
-
-    &:hover {
-      background-color: ${(props) => props.theme.css.lightVariantColor};
-      cursor: pointer;
-
-      svg {
-        background-color: white;
-        color: ${(props) => props.theme.css.activeColor};
-        border-radius: 1em;
-      }
-    }
-  }
-
   .form-footer {
     // It overlaps the bottom row and it's impossible to scroll further.
     /* position: fixed; */
@@ -108,5 +30,67 @@ export const TopicScoreRuleList = styled.div`
     width: 100%;
     background-color: ${(props) => props.theme.css.backgroundColor};
     z-index: 2;
+  }
+
+  .grid-table {
+    display: grid;
+    grid-template-columns: 3fr 3fr 2fr repeat(9, 1fr);
+    align-items: center;
+    grid-gap: 0.25em;
+
+    > div {
+      &.center {
+        justify-self: center;
+      }
+
+      .text-right {
+        text-align: right;
+      }
+    }
+
+    .grid-row {
+      display: contents;
+
+      > div {
+        .frm-in {
+          padding: 0;
+        }
+
+        &.center {
+          justify-self: center;
+        }
+      }
+
+      button.move {
+        height: 40%;
+
+        padding: 0;
+        margin-right: 0.5em;
+
+        svg {
+          height: 100%;
+        }
+      }
+    }
+
+    .add-row {
+      grid-column: 1/13;
+      background-color: ${(props) => props.theme.css.borderColor};
+      height: 5px;
+      align-content: center;
+      justify-content: center;
+      z-index: 2;
+
+      &:hover {
+        background-color: ${(props) => props.theme.css.lightVariantColor};
+        cursor: pointer;
+
+        svg {
+          background-color: white;
+          color: ${(props) => props.theme.css.activeColor};
+          border-radius: 1em;
+        }
+      }
+    }
   }
 `;
--- app/editor/src/store/hooks/editor/useContent.ts
+++ app/editor/src/store/hooks/editor/useContent.ts
@@ -2,7 +2,7 @@ import {
   IContentListAdvancedFilter,
   IContentListFilter,
 } from 'features/content/list-view/interfaces';
-import { IMorningReportsFilter } from 'features/content/morning-reports/interfaces';
+import { IMorningReportsFilter } from 'features/content/morning-papers/interfaces';
 import React from 'react';
 import { ActionDelegate } from 'store';
 import { useContentStore } from 'store/slices';
