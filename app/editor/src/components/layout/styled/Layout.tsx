2253fc593c445fe22fc07a6bf8cb2a97064f36b9
Cello Liu <liu.cello@gmail.com>
2023-04-26 12:21:01 -0700

    TNO-1172 updates #2.1

--- app/editor/src/components/layout/styled/Layout.tsx
+++ app/editor/src/components/layout/styled/Layout.tsx
@@ -35,9 +35,10 @@ export const Layout = styled.div`
   & > header {
     background-color: ${(props) => {
       const isDevelopment =
-        window.location.href.includes(env['dev-env']) || process.env.NODE_ENV === 'development';
+        window.location.href.includes(env.environments[0]) ||
+        process.env.NODE_ENV === 'development';
       const isTest =
-        window.location.href.includes(env['test-env']) || process.env.NODE_ENV === 'test';
+        window.location.href.includes(env.environments[1]) || process.env.NODE_ENV === 'test';
 
       if (isDevelopment) return props.theme.css.developmentBackgroundColor;
       else if (isTest) return props.theme.css.testBackgroundColor;
