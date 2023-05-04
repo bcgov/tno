02ff877b01c4592a71fe22506ac320ebfa842b77
Cello Liu <55408610+CelloLiu@users.noreply.github.com>
2023-05-04 14:03:14 -0700

    TNO-1199 (#699)
    * TNO-1199
    
    * TNO-1199 (MMIA related updates)
    
    * TNO-1199 PR updates

--- services/net/notification/Models/TemplateModel.cs
+++ services/net/notification/Models/TemplateModel.cs
@@ -1,12 +1,24 @@
-﻿using TNO.API.Areas.Services.Models.Content;
+﻿using Microsoft.Extensions.Options;
+using TNO.API.Areas.Services.Models.Content;
+using TNO.Services.Notification.Config;
 
 namespace TNO.Services.Notification.Models
 {
     public class TemplateModel
     {
+        public TemplateModel(IOptions<NotificationOptions> notificationOptions)
+        {
+            NotificationOptions = notificationOptions;
+        }
+
         /// <summary>
         /// get/set - The content model.
         /// </summary>
         public ContentModel Content { get; set; } = new ContentModel();
+
+        /// <summary>
+        /// get - Notification options.
+        /// </summary>
+        public IOptions<NotificationOptions> NotificationOptions { get; }
     }
 }
