83f2927077730b56665d0494cd557a233f50e357
jdtoombs <jdtoombs@gmail.com>
2023-05-08 10:32:34 -0700

    Fix columns

--- app/subscriber/src/features/home/constants/columns.tsx
+++ app/subscriber/src/features/home/constants/columns.tsx
@@ -6,19 +6,21 @@ export const columns: ITableHookColumn<IContentModel>[] = [
   {
     name: 'tone',
     label: 'TONE',
-    cell: (cell: any) => <DetermineToneIcon tone={cell.original.tonePools[0]?.value ?? 0} />,
+    cell: (cell) => (
+      <DetermineToneIcon tone={cell.original.tonePools ? cell.original.tonePools[0]?.value : 0} />
+    ),
   },
   {
     name: 'headline',
     label: 'HEADLINE',
-    cell: (cell) => <div className="headline">{cell?.original.headline}</div>,
+    cell: (cell) => <div className="headline">{cell.original.headline}</div>,
     width: 5,
   },
   {
     name: 'sectionPage',
     label: 'SECTION PAGE',
-    cell: (cell: any) => (
-      <div className="section">{`${cell?.original.section}:${cell?.original.page}`}</div>
+    cell: (cell) => (
+      <div className="section">{`${cell.original.section}:${cell?.original.page}`}</div>
     ),
     width: 1,
   },
