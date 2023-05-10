d106b56e40ac56ae905daacd3af2cebafc7675c3
Jeremy Foster <jeremy.foster@fosol.ca>
2023-05-10 13:17:29 -0700

    TNO-1170 Fix select component Publish tno-core:0.0.244 Fix yup validation

--- app/editor/src/features/content/validation/ContentFormSchema.ts
+++ app/editor/src/features/content/validation/ContentFormSchema.ts
@@ -4,42 +4,42 @@ import { array, date, number, object, string } from 'yup';
 export const ContentFormSchema = object().shape(
   {
     productId: number().required('Product is required.').notOneOf([0], 'Product is required.'),
-    sourceId: number().when('tempSource', (value: any) => {
-      if (value === undefined)
+    sourceId: number().when('tempSource', (value: (number | undefined)[]) => {
+      if (value[0] === undefined)
         return number().required('Either source or other source is required.');
       return number();
     }),
-    tempSource: string().when('sourceId', (value: any) => {
-      if (value === undefined)
+    tempSource: string().when('sourceId', (value: (string | undefined)[]) => {
+      if (value[0] === undefined)
         return string().required('Either source or other source is required.');
       return string();
     }),
     publishedOn: date().required('Published On is a required field.'),
     // TODO: Summary should not be empty.
-    summary: string().when('contentType', (value: any) => {
-      if (value === ContentTypeName.Snippet)
+    summary: string().when('contentType', (value: string[]) => {
+      if (value[0] === ContentTypeName.Snippet)
         return string().trim().required('Summary is a required field.');
       return string();
     }),
-    body: string().when('contentType', (value: any) => {
-      if (value !== ContentTypeName.Snippet && value !== ContentTypeName.Image)
+    body: string().when('contentType', (value: string[]) => {
+      if (value[0] !== ContentTypeName.Snippet && value[0] !== ContentTypeName.Image)
         return string().trim().required('Summary is a required field.');
       return string();
     }),
     // TODO: Headline should not be empty.
     headline: string().required('Headline is a required field.'),
-    tonePools: string().when('contentType', (value: any) => {
-      if (value !== ContentTypeName.Image) return array().min(1, 'Tone is a required field.');
+    tonePools: string().when('contentType', (value: string[]) => {
+      if (value[0] !== ContentTypeName.Image) return array().min(1, 'Tone is a required field.');
       return array();
     }),
     // TODO: validation for print content.
-    section: string().when('contentType', (value: any) => {
-      if (value === ContentTypeName.PrintContent)
+    section: string().when('contentType', (value: string[]) => {
+      if (value[0] === ContentTypeName.PrintContent)
         return string().trim().required('Section is a required field.');
       return string();
     }),
-    byline: string().when('contentType', (value: any) => {
-      if (value === ContentTypeName.PrintContent)
+    byline: string().when('contentType', (value: string[]) => {
+      if (value[0] === ContentTypeName.PrintContent)
         return string().trim().required('Byline is a required field.');
       return string();
     }),
