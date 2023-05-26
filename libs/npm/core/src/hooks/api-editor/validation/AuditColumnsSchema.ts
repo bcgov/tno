8a37eca1d5e405fa5ab3a7ea423f39833eaa3497
jdtoombs <jdtoombs@gmail.com>
2023-05-25 17:02:24 -0700

    Fix npm/core build errors

--- libs/npm/core/src/hooks/api-editor/validation/AuditColumnsSchema.ts
+++ libs/npm/core/src/hooks/api-editor/validation/AuditColumnsSchema.ts
@@ -5,7 +5,7 @@ import { IAuditColumnsModel } from '../';
 /**
  * Validation schema for audit columns.
  */
-export const AuditColumnsSchema: Yup.ObjectSchema<IAuditColumnsModel> = Yup.object().shape({
+export const AuditColumnsSchema: Yup.SchemaOf<IAuditColumnsModel> = Yup.object().shape({
   createdOn: Yup.string(),
   createdBy: Yup.string(),
   createdById: Yup.string(),
