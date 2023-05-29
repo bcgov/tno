05ddcb7aff65dcf5f8225b098ffb7eb10d009001
Jeremy Foster <jeremy.foster@fosol.ca>
2023-05-29 15:25:18 -0700

    TNO-1217 - Add admin report subscribers (#765)

---
+++ app/editor/src/features/admin/reports/constants/subscriberColumns.tsx
@@ -0,0 +1,52 @@
+import { CellEllipsis, Checkbox, IReportModel, ITableHookColumn, IUserModel } from 'tno-core';
+
+export const subscriberColumns = (
+  report: IReportModel,
+  setFieldValue: (field: string, value: any, shouldValidate?: boolean | undefined) => void,
+): ITableHookColumn<IUserModel>[] => [
+  {
+    label: '',
+    name: 'id',
+    width: '50px',
+    cell: (cell) => (
+      <Checkbox
+        id={`user-${cell.original.id}`}
+        value={true}
+        checked={report.subscribers.some((u) => u.id === cell.original.id)}
+        onChange={(e) => {
+          if (e.target.checked)
+            setFieldValue('subscribers', [cell.original, ...report.subscribers]);
+          else
+            setFieldValue(
+              'subscribers',
+              report.subscribers.filter((s) => s.id !== cell.original.id),
+            );
+        }}
+      />
+    ),
+  },
+  {
+    label: 'Username',
+    name: 'username',
+    width: 1,
+    cell: (cell) => <CellEllipsis>{cell.original.username}</CellEllipsis>,
+  },
+  {
+    label: 'Last Name',
+    name: 'lastName',
+    width: 1,
+    cell: (cell) => <CellEllipsis>{cell.original.lastName}</CellEllipsis>,
+  },
+  {
+    label: 'First Name',
+    name: 'firstName',
+    width: 1,
+    cell: (cell) => <CellEllipsis>{cell.original.firstName}</CellEllipsis>,
+  },
+  {
+    label: 'Email',
+    name: 'email',
+    width: 2,
+    cell: (cell) => <CellEllipsis>{cell.original.email}</CellEllipsis>,
+  },
+];
