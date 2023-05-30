27a356ca6bfbb08cea1c40652d65dc48c464923b
jdtoombs <jdtoombs@gmail.com>
2023-05-30 10:43:50 -0700

    Feedback

--- app/subscriber/src/features/home/constants/columns.tsx
+++ app/subscriber/src/features/home/constants/columns.tsx
@@ -3,51 +3,36 @@ import { ContentTypeName, IContentModel, ITableHookColumn } from 'tno-core';
 import { DetermineToneIcon } from '../utils';
 
 export const determinecolumns = (contentType: ContentTypeName) => {
-  let cols: ITableHookColumn<IContentModel>[] = [];
+  // columns common to all content
+  const baseCols: ITableHookColumn<IContentModel>[] = [
+    {
+      name: 'tone',
+      label: 'TONE',
+      cell: (cell) => (
+        <DetermineToneIcon tone={cell.original.tonePools ? cell.original.tonePools[0]?.value : 0} />
+      ),
+    },
+    {
+      name: 'headline',
+      label: 'HEADLINE',
+      cell: (cell) => <div className="headline">{cell.original.headline}</div>,
+      width: 5,
+    },
+  ];
+  // columns specific to print content
+  const printCols: ITableHookColumn<IContentModel>[] = [
+    {
+      name: 'sectionPage',
+      label: 'SECTION PAGE',
+      cell: (cell) => (
+        <div className="section">{`${cell.original.section}: ${cell?.original.page}`}</div>
+      ),
+      width: 1,
+    },
+  ];
   if (contentType === ContentTypeName.PrintContent) {
-    cols = [
-      {
-        name: 'tone',
-        label: 'TONE',
-        cell: (cell) => (
-          <DetermineToneIcon
-            tone={cell.original.tonePools ? cell.original.tonePools[0]?.value : 0}
-          />
-        ),
-      },
-      {
-        name: 'headline',
-        label: 'HEADLINE',
-        cell: (cell) => <div className="headline">{cell.original.headline}</div>,
-        width: 5,
-      },
-      {
-        name: 'sectionPage',
-        label: 'SECTION PAGE',
-        cell: (cell) => (
-          <div className="section">{`${cell.original.section}: ${cell?.original.page}`}</div>
-        ),
-        width: 1,
-      },
-    ];
+    return [...baseCols, ...printCols];
   } else {
-    cols = [
-      {
-        name: 'tone',
-        label: 'TONE',
-        cell: (cell) => (
-          <DetermineToneIcon
-            tone={cell.original.tonePools ? cell.original.tonePools[0]?.value : 0}
-          />
-        ),
-      },
-      {
-        name: 'headline',
-        label: 'HEADLINE',
-        cell: (cell) => <div className="headline">{cell.original.headline}</div>,
-        width: 5,
-      },
-    ];
+    return baseCols;
   }
-  return cols;
 };
