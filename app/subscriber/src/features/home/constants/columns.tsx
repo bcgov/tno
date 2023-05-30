17a013a5342e048c6567de8ce60fb49beb46a0ab
Jonny Toombs <jdtoombs@gmail.com>
2023-05-30 11:25:37 -0700

    TNO-1289: Page/section should only show on print (#769)
    * TNO-1289: Page/section should only show on print
    
    * Feedback

--- app/subscriber/src/features/home/constants/columns.tsx
+++ app/subscriber/src/features/home/constants/columns.tsx
@@ -1,27 +1,38 @@
-import { IContentModel, ITableHookColumn } from 'tno-core';
+import { ContentTypeName, IContentModel, ITableHookColumn } from 'tno-core';
 
 import { DetermineToneIcon } from '../utils';
 
-export const columns: ITableHookColumn<IContentModel>[] = [
-  {
-    name: 'tone',
-    label: 'TONE',
-    cell: (cell) => (
-      <DetermineToneIcon tone={cell.original.tonePools ? cell.original.tonePools[0]?.value : 0} />
-    ),
-  },
-  {
-    name: 'headline',
-    label: 'HEADLINE',
-    cell: (cell) => <div className="headline">{cell.original.headline}</div>,
-    width: 5,
-  },
-  {
-    name: 'sectionPage',
-    label: 'SECTION PAGE',
-    cell: (cell) => (
-      <div className="section">{`${cell.original.section}: ${cell?.original.page}`}</div>
-    ),
-    width: 1,
-  },
-];
+export const determinecolumns = (contentType: ContentTypeName) => {
+  // columns common to all content
+  const baseCols: ITableHookColumn<IContentModel>[] = [
+    {
+      name: 'tone',
+      label: 'TONE',
+      cell: (cell) => (
+        <DetermineToneIcon tone={cell.original.tonePools ? cell.original.tonePools[0]?.value : 0} />
+      ),
+    },
+    {
+      name: 'headline',
+      label: 'HEADLINE',
+      cell: (cell) => <div className="headline">{cell.original.headline}</div>,
+      width: 5,
+    },
+  ];
+  // columns specific to print content
+  const printCols: ITableHookColumn<IContentModel>[] = [
+    {
+      name: 'sectionPage',
+      label: 'SECTION PAGE',
+      cell: (cell) => (
+        <div className="section">{`${cell.original.section}: ${cell?.original.page}`}</div>
+      ),
+      width: 1,
+    },
+  ];
+  if (contentType === ContentTypeName.PrintContent) {
+    return [...baseCols, ...printCols];
+  } else {
+    return baseCols;
+  }
+};
