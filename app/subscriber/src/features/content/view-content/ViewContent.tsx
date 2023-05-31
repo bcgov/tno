fb9f843be84122cbb3922e13edd61d39b38ea56d
jdtoombs <jdtoombs@gmail.com>
2023-05-31 13:45:20 -0700

    TNO-1286: Format time w/ 2 digits

--- app/subscriber/src/features/content/view-content/ViewContent.tsx
+++ app/subscriber/src/features/content/view-content/ViewContent.tsx
@@ -7,6 +7,8 @@ import { ContentTypeName, IContentModel, Row, Show, useWindowSize } from 'tno-co
 
 import * as styled from './styled';
 import { ViewContentToolbar } from './ViewContentToolbar';
+import { format } from 'path';
+import { formatTime } from './utils';
 
 export interface IStream {
   url: string;
@@ -66,7 +68,7 @@ export const ViewContent: React.FC = () => {
       const ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
       const mo = new Intl.DateTimeFormat('en', { month: 'short' }).format(d);
       const da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d);
-      return `${da}-${mo}-${ye} ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
+      return `${da}-${mo}-${ye} ${formatTime(d)}`;
     } else {
       return '';
     }
---
+++ app/subscriber/src/features/content/view-content/utils/formatTime.ts
@@ -0,0 +1,6 @@
+export const formatTime = (date: Date) => {
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+  return `${hours}:${minutes}:${seconds}`;
+};
