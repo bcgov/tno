163a792f0ad4c148bbfa99a67d0452993bf620bb
Cello Liu <liu.cello@gmail.com>
2023-06-19 16:10:20 -0700

    TNO-1407 PR updates

--- app/editor/src/features/admin/topics/TopicList.tsx
+++ app/editor/src/features/admin/topics/TopicList.tsx
@@ -51,7 +51,7 @@ export const TopicList: React.FC = () => {
         .findAllTopics()
         .then((data) => {
           setTopics(data.filter((t) => t.id !== 1));
-          setItems(data.filter((t) => t.id !== 1));
+          setItems(data.filter((t) => t.id !== 1 && t.isEnabled));
         })
         .finally(() => {
           setLoading(false);
@@ -68,8 +68,20 @@ export const TopicList: React.FC = () => {
     try {
       var results = [...items];
       if (values.id === 0) {
-        const result = await api.addTopic(values);
-        results = [...items, result];
+        const topic = topics.find((x) => x.name === values.name);
+        if (!topic) {
+          const result = await api.addTopic(values);
+          results = [...items, result];
+        } else {
+          const result = await api.updateTopic({
+            ...topic,
+            isEnabled: values.isEnabled,
+            description: values.description,
+            topicType: values.topicType,
+          });
+          if (topic.isEnabled) results = items.map((i) => (i.id === result.id ? result : i));
+          else if (values.isEnabled) results = [...items, result];
+        }
       } else {
         const result = await api.updateTopic(values);
         results = items.map((i) => (i.id === values.id ? result : i));
@@ -93,13 +105,14 @@ export const TopicList: React.FC = () => {
                   setItems(
                     topics.filter(
                       (i) =>
-                        i.name.toLocaleLowerCase().includes(value) ||
-                        i.description.toLocaleLowerCase().includes(value) ||
-                        i.topicType.toLocaleLowerCase().includes(value),
+                        i.isEnabled &&
+                        (i.name.toLocaleLowerCase().includes(value) ||
+                          i.description.toLocaleLowerCase().includes(value) ||
+                          i.topicType.toLocaleLowerCase().includes(value)),
                     ),
                   );
                 } else {
-                  setItems(topics);
+                  setItems(topics.filter((x) => x.isEnabled));
                 }
               }}
             />
@@ -155,7 +168,7 @@ export const TopicList: React.FC = () => {
                     variant={ButtonVariant.danger}
                     disabled={isSubmitting || !values.id}
                     onClick={async () => {
-                      await api.deleteTopic(topic);
+                      await api.updateTopic({ ...topic, isEnabled: false });
                       setItems(items.filter((t) => t.id !== topic.id));
                       toast.success(`${topic.name} has successfully been deleted.`);
                       navigate('/admin/topics');
