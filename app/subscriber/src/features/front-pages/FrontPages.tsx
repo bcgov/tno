39dfeac52f7c776e14f9aa3fd33d11b98868cc67
jdtoombs <jdtoombs@gmail.com>
2023-06-26 16:11:39 -0700

    lint

--- app/subscriber/src/features/front-pages/FrontPages.tsx
+++ app/subscriber/src/features/front-pages/FrontPages.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useContent } from 'store/hooks';
-import { ContentTypeName, IContentModel, Row } from 'tno-core';
+import { IContentModel, Row } from 'tno-core';
 
 import * as styled from './styled';
 
