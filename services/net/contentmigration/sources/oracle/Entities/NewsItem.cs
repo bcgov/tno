36356f62cd8faed995072851aeddd78d4c0a27ba
kylgarmor <kyle@nexusfusion.com>
2023-06-28 13:41:03 -0700

    feat: #tno-14745 - using a union all to solve - trying a UNION ALL query inside a CTE on the EntityBuilder

--- services/net/contentmigration/sources/oracle/Entities/NewsItem.cs
+++ services/net/contentmigration/sources/oracle/Entities/NewsItem.cs
@@ -5,7 +5,7 @@ namespace TNO.Services.ContentMigration.Sources.Oracle;
 /// <summary>
 /// NewsItem class, provides an entity to store News Item records in the database.
 /// </summary>
-[Table("NEWS_ITEMS")]
+// [Table("NEWS_ITEMS")]
 public class NewsItem
 {
     /// <summary>
