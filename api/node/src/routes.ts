79e61ffa8e0a9ba4b90685dcb4c8c97c01fc3d9c
Jeremy Foster <jeremy.foster@fosol.ca>
2023-07-07 12:49:00 -0700

    TNO-1487 Add charts API

--- api/node/src/routes.ts
+++ api/node/src/routes.ts
@@ -3,7 +3,6 @@ import { CategoryScale, Colors } from 'chart.js';
 import Chart from 'chart.js/auto';
 import { getString, convertChartJsConfigToBase64String, generateBase64, sendImage } from './utils';
 import { ChartTypes } from './charts';
-import { data } from './data/data';
 
 Chart.register(CategoryScale);
 Chart.defaults.animation = false;
@@ -13,9 +12,14 @@ Chart.register(Colors);
 
 const routes = Router();
 
-/** Temporary endpoint to provide test data. */
-routes.get('/data', (req, res) => {
-  const base64 = convertChartJsConfigToBase64String(data);
+/** Readiness and liveness probe endpoint. */
+routes.get('/health', (req, res) => {
+  res.sendStatus(200);
+});
+
+/** Convert body to base64 string. */
+routes.post('/data', (req, res) => {
+  const base64 = convertChartJsConfigToBase64String(req.body);
   res.send({ data: base64 });
 });
 
@@ -31,6 +35,18 @@ routes.get('/graph/:type', (req, res) => {
   return sendImage(type, req, res);
 });
 
+/** Draw a chart on a canvas and return an image. */
+routes.post('/graph', async (req, res) => {
+  const type = (getString(req, 'type') as ChartTypes) ?? ChartTypes.bar;
+  return sendImage(type, req, res);
+});
+
+/** Draw a chart on a canvas and return an image. */
+routes.post('/graph/:type', (req, res) => {
+  const type = req.params.type as ChartTypes;
+  return sendImage(type, req, res);
+});
+
 /** Draw a chart on a canvas and return a base64 string */
 routes.get('/base64', async (req, res) => {
   const type = (getString(req, 'type') as ChartTypes) ?? ChartTypes.bar;
@@ -39,6 +55,18 @@ routes.get('/base64', async (req, res) => {
 
 /** Draw a chart on a canvas and return a base64 string */
 routes.get('/base64/:type', (req, res) => {
+  const type = req.params.type as ChartTypes;
+  return res.send(generateBase64(type, req));
+});
+
+/** Draw a chart on a canvas and return a base64 string */
+routes.post('/base64', async (req, res) => {
+  const type = (getString(req, 'type') as ChartTypes) ?? ChartTypes.bar;
+  return res.send(generateBase64(type, req));
+});
+
+/** Draw a chart on a canvas and return a base64 string */
+routes.post('/base64/:type', (req, res) => {
   const type = req.params.type as ChartTypes;
   return res.send(generateBase64(type, req));
 });
