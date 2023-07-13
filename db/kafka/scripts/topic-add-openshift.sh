07a62206ebb119b544ebf57d7760942558b58c6c
kulpreet Singh <kulpreetsingh.acme@gmail.com>
2023-07-13 09:46:53 -0600

    feat(TNO-1401 - Openshift kustomize code optimization) (#897)
    * feat(TNO-1401): Deploying to production and optimizing the tno resource  deployment
    
    * fix(Dev Env deployment): Created separate file for Dec compose & OC environment
    
    * fix(ImagePull - tools): Fixed the image pull back to tools from prod.

---
+++ db/kafka/scripts/topic-add-openshift.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+#################################################
+# Arguments
+#################################################
+
+# More safety, by turning some bugs into errors.
+# Without `errexit` you don’t need ! and can replace
+# PIPESTATUS with a simple $?, but I don’t do that.
+set -o errexit -o pipefail -o noclobber -o nounset
+
+# -allow a command to fail with !’s side effect on errexit
+# -use return value from ${PIPESTATUS[0]}, because ! hosed $?
+! getopt --test > /dev/null
+if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
+    echo 'I’m sorry, `getopt --test` failed in this environment.'
+    exit 1
+fi
+
+OPTIONS=e:t:b:p:r:i:
+LONGOPTS=environment:,topic:,bootstrap:,partitions:,replication:,index:
+
+# -regarding ! and PIPESTATUS see above
+# -temporarily store output to be able to check for errors
+# -activate quoting/enhanced mode (e.g. by writing out “--options”)
+# -pass arguments only via   -- "$@"   to separate them correctly
+! PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
+if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
+    # e.g. return value is 1
+    #  then getopt has complained about wrong arguments to stdout
+    exit 2
+fi
+# read getopt’s output this way to handle the quoting right:
+eval set -- "$PARSED"
+
+# Default values.
+environment=local bootstrap=${BOOTSTRAP:-kafka-broker-0:29094} partitions=3 replication=3 index=0
+
+# now enjoy the options in order and nicely split until we see --
+while true; do
+  case "$1" in
+    -e|--environment)
+      environment="$2"
+      shift 2
+      ;;
+    -t|--topic)
+      topic="$2"
+      shift 2
+      ;;
+    -b|--bootstrap)
+      bootstrap="$2"
+      shift 2
+      ;;
+    -p|--partitions)
+      partitions="$2"
+      shift 2
+      ;;
+    -r|--replication)
+      replication="$2"
+      shift 2
+      ;;
+    -i|--index)
+      index="$2"
+      shift 2
+      ;;
+    --)
+      shift
+      break
+      ;;
+    *)
+      echo "Programming error"
+      exit 3
+      ;;
+  esac
+done
+
+if [ "$environment" = "local" ]; then
+  bootstrap=${BOOTSTRAP:-broker:29092} partitions=1 replication=1
+fi
+
+if [ -z "$topic" ]; then
+    echo "Enter a topic name."
+    read -p 'Topic: ' topic
+fi
+
+if [ -z "$bootstrap" ]; then
+    echo "Enter the host and port to the bootstrap server."
+    read -p 'Host and Port: ' bootstrap
+fi
+
+echo "environment: $environment, topic: $topic, bootstrap: $bootstrap, partitions: $partitions, replication: $replication, index: $index"
+
+# Make variables available scripts.
+export environment
+export topic
+export bootstrap
+export partitions
+export replication
+export index
+
+#################################################
+# Work
+#################################################
+
+if [ "$environment" = "local" ]; then
+  docker exec -i tno-broker bash -c "/bin/kafka-topics --create --topic $topic --bootstrap-server $bootstrap --partitions $partitions --replication-factor $replication"
+else
+  oc rsh -n 9b301c-$environment kafka-broker-$index bash -c "/bin/kafka-topics --create --topic $topic --bootstrap-server $bootstrap --partitions $partitions --replication-factor $replication"
+fi
---
+++ openshift/scripts/create-environment.sh
@@ -0,0 +1,57 @@
+# Create policy for prod to pull images
+oc policy add-role-to-user system:image-puller system:serviceaccount:9b301c-prod:default -n 9b301c-tools
+
+# Tag Imagestreams and import them to prod.
+oc -n 9b301c-prod tag 9b301c-tools/zookeeper:dev zookeeper:prod
+oc -n 9b301c-prod tag 9b301c-tools/kafka:dev kafka:prod
+
+# Deploy statefulSets to the prod environment
+
+oc kustomize kafka/zookeeper/overlays/prod | oc create -f -
+oc kustomize kafka/zookeeper/overlays/prod | oc create -f -
+oc kustomize kafka/rest-proxy/overlays/prod | oc create -f -
+oc kustomize postgres/crunchy/overlays/prod | oc create -f -
+oc kustomize elastic/overlays/prod | oc create -f -
+
+# Create kafka topic
+
+oc rsh -n 9b301c-prod kafka-broker-0 bash -c "/bin/kafka-topics --bootstrap-server kafka-broker-0.kafka-headless:9092,kafka-broker-1.kafka-headless:9092,kafka-broker-2.kafka-headless:9092 --topic hub --create --partitions 3 --replication-factor 1"
+
+# Once the database, Kafka, and Elasticsearch are running successfully.
+# Deploy the application components.
+
+# Nginx pull image & deploy nginx
+oc -n 9b301c-tools tag 9b301c-tools/nginx:dev nginx:prod
+oc kustomize nginx/overlays/prod | oc create -f -
+
+# API Image tag & deploy API
+oc -n 9b301c-tools tag 9b301c-tools/api:dev api:prod
+oc kustomize api/overlays/prod | oc create -f -
+oc kustomize app/editor/overlays/prod | oc create -f -
+
+# Deploy shared services resources - This will create ingest-storage pvc & services configmap.
+
+oc kustomize shared_resources/overlays/prod | oc create -f -
+
+# Tag all app image for prod
+oc -n 9b301c-tools tag 9b301c-tools/syndication-service:dev syndication-service:prod
+oc -n 9b301c-tools tag 9b301c-tools/capture-service:dev capture-service:prod
+oc -n 9b301c-tools tag 9b301c-tools/clip-service:dev clip-service:prod
+oc -n 9b301c-tools tag 9b301c-tools/image-service:dev image-service:prod
+oc -n 9b301c-tools tag 9b301c-tools/filemonitor-service:dev filemonitor-service:prod
+oc -n 9b301c-tools tag 9b301c-tools/content-service:dev content-service:prod
+oc -n 9b301c-tools tag 9b301c-tools/indexing-service:dev indexing-service:prod
+oc -n 9b301c-tools tag 9b301c-tools/transcription-service:dev transcription-service:prod
+oc -n 9b301c-tools tag 9b301c-tools/nlp-service:dev nlp-service:prod
+
+
+# Deploy images to prod.
+oc kustomize services/syndication/overlays/prod | oc create -f -
+oc kustomize services/capture/overlays/prod | oc create -f -
+oc kustomize services/clip/overlays/prod | oc create -f -
+oc kustomize services/image/overlays/prod | oc create -f -
+oc kustomize services/filemonitor/overlays/prod | oc create -f -
+oc kustomize services/content/overlays/prod | oc create -f -
+oc kustomize services/indexing/overlays/prod | oc create -f -
+oc kustomize services/transcription/overlays/prod | oc create -f -
+oc kustomize services/nlp/overlays/prod | oc create -f -
