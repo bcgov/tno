b48d2b80269568b8dfc731936c8d0df6e48d6dee
kylgarmor <kyle@nexusfusion.com>
2023-07-24 09:51:34 -0700

    fix: #tno-1492 - increased column size for Page - increased from 10 to 20

---
+++ libs/net/dal/Migrations/20230724165029_1.0.50.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using TNO.DAL;
+
+#nullable disable
+
+namespace TNO.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class _1050 : SeedMigration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            PreUp(migrationBuilder);
+            migrationBuilder.AlterColumn<string>(
+                name: "page",
+                table: "content",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(10)",
+                oldMaxLength: 10);
+            PostUp(migrationBuilder);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            PreDown(migrationBuilder);
+            migrationBuilder.AlterColumn<string>(
+                name: "page",
+                table: "content",
+                type: "character varying(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(20)",
+                oldMaxLength: 20);
+            PostDown(migrationBuilder);
+        }
+    }
+}
