111ac253cc6960bbdd722d58f4beddb484aff46c
jdtoombs <jdtoombs@gmail.com>
2023-07-24 13:55:04 -0700

    cleanup

--- app/subscriber/src/store/hooks/subscriber/interfaces/IMinisterModel.ts
+++ app/subscriber/src/store/hooks/subscriber/interfaces/IMinisterModel.ts
@@ -2,7 +2,6 @@ import { IOrganizationModel, ISortableModel } from 'tno-core';
 
 export interface IMinisterModel extends ISortableModel<number> {
   aliases: string;
-  description: string;
   organizationId?: number;
   organization?: IOrganizationModel;
   position: string;
