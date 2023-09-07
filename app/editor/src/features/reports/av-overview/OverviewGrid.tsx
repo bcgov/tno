a04614ebde0b4f659e2ed73e0b2106b90bb23d6b
jdtoombs <jdtoombs@gmail.com>
2023-09-07 13:45:40 -0700

    Lint

--- app/editor/src/features/reports/av-overview/OverviewGrid.tsx
+++ app/editor/src/features/reports/av-overview/OverviewGrid.tsx
@@ -45,7 +45,7 @@ export const OverviewGrid: React.FC<IOverviewGridProps> = ({ editable = true, in
     }).then((data) =>
       setClips(data.items.map((c) => new OptionItem(c.headline, c.id)) as IOptionItem[]),
     );
-  }, [findContent, values.sections[index].seriesId]);
+  }, [findContent, index, values.sections]);
 
   /** function that runs after a user drops an item in the list */
   const handleDrop = (droppedItem: any) => {
