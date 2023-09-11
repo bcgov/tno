4185c768f638c05e893a73c911b1510d6f1647aa
jdtoombs <jdtoombs@gmail.com>
2023-09-11 10:46:30 -0700

    Add settings instead of json

--- app/editor/src/features/content/form/components/tool-bar/form/PublishedSection.tsx
+++ app/editor/src/features/content/form/components/tool-bar/form/PublishedSection.tsx
@@ -1,6 +1,8 @@
 import { IContentForm } from 'features/content/form/interfaces';
 import React from 'react';
 import { FaExternalLinkAlt } from 'react-icons/fa';
+import { toast } from 'react-toastify';
+import { useSettings } from 'store/hooks/admin';
 import { Col, Row } from 'tno-core';
 
 import * as styled from './styled';
@@ -11,22 +13,30 @@ export interface IPublishedSectionProps {
 }
 
 export const PublishedSection: React.FC<IPublishedSectionProps> = ({ values }) => {
-  const [subscriberLink, setSubscriberLink] = React.useState<string>('');
+  const [subscriberUrl, setSubscriberUrl] = React.useState<string>();
+  const [{ settings }, api] = useSettings();
+
   React.useEffect(() => {
-    fetch('/links.json')
-      .then((res) => res.json())
-      .then((data) => {
-        if (process.env.NODE_ENV === 'development') setSubscriberLink(data.localSubscriber);
-        if (process.env.NODE_ENV === 'test') setSubscriberLink(data.devSubscriber);
-        if (process.env.NODE_ENV === 'production') setSubscriberLink(data.prodSubscriber);
-      });
-  }, []);
+    api.findAllSettings().then((data) => {
+      setSubscriberUrl(data.find((i) => i.name === 'SubscriberUrl')?.value);
+    });
+  }, [api, settings, subscriberUrl]);
+
   return (
     <styled.PublishedSection
       label="VIEW ON MMIA"
       children={
         <Col>
-          <Row className="view" onClick={() => window.open(`${subscriberLink}/view/${values.id}`)}>
+          <Row
+            className="view"
+            onClick={() => {
+              if (!subscriberUrl) {
+                toast.error('SubscriberUrl setting not found. Please set and try again.');
+                return;
+              }
+              window.open(`${subscriberUrl}/view/${values.id}`);
+            }}
+          >
             <FaExternalLinkAlt /> View on site
           </Row>
         </Col>
