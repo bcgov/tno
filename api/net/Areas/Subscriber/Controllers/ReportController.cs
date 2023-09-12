5c488b727faf3f8a305f17521c76b1756c9ff207
Jeremy Foster <jeremy.foster@fosol.ca>
2023-09-12 09:26:44 -0700

    TNO-1723 Enable report preview

--- api/net/Areas/Subscriber/Controllers/ReportController.cs
+++ api/net/Areas/Subscriber/Controllers/ReportController.cs
@@ -212,7 +212,9 @@ public class ReportController : ControllerBase
         var username = User.GetUsername() ?? throw new NotAuthorizedException("Username is missing");
         var user = _userService.FindByUsername(username) ?? throw new NotAuthorizedException("User does not exist");
         var report = _reportService.FindById(id) ?? throw new InvalidOperationException("Report does not exist");
-        if (report.OwnerId != user?.Id && !report.IsPublic) throw new NotAuthorizedException("Not authorized to preview this report");
+        if (report.OwnerId != user.Id && // User does not own the report
+            !report.SubscribersManyToMany.Any(s => s.IsSubscribed && s.UserId == user.Id) &&  // User is not subscribed to the report
+            !report.IsPublic) throw new NotAuthorizedException("Not authorized to preview this report"); // Report is not public
         var model = new Services.Models.Report.ReportModel(report, _serializerOptions);
         var result = await _reportHelper.GenerateReportAsync(model);
         return new JsonResult(result);
