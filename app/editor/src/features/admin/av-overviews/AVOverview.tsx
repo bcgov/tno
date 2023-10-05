c9c1d9d5e4790a3eb7d764df9fc339ca9eb89360
Cello Liu <liu.cello@gmail.com>
2023-10-04 18:00:51 -0700

    TNO-1694: Admin Evening Overview Subscribers

--- app/editor/src/features/admin/av-overviews/AVOverview.tsx
+++ app/editor/src/features/admin/av-overviews/AVOverview.tsx
@@ -2,6 +2,7 @@ import { FormikForm } from 'components/formik';
 import React from 'react';
 import { FaSpinner } from 'react-icons/fa';
 import { MdAdd, MdSave } from 'react-icons/md';
+import { toast } from 'react-toastify';
 import { useAVOverviewTemplates, useReportTemplates } from 'store/hooks/admin';
 import {
   AVOverviewTemplateTypeName,
@@ -11,6 +12,8 @@ import {
   Row,
   Select,
   Show,
+  Tab,
+  Tabs,
 } from 'tno-core';
 
 import {
@@ -21,6 +24,7 @@ import {
 } from './constants';
 import { IAVOverviewTemplateForm } from './interfaces';
 import { OverviewSection } from './OverviewSection';
+import { OverviewSubscribers } from './OverviewSubscribers';
 import * as styled from './styled';
 
 /** Evening overview section, contains table of items, and list of overview sections */
@@ -32,6 +36,8 @@ const AVOverview: React.FC = () => {
   const [template, setTemplate] = React.useState<IAVOverviewTemplateForm>(
     defaultAVOverviewTemplate(AVOverviewTemplateTypeName.Weekday, 0),
   );
+
+  const [active, setActive] = React.useState('report');
 
   const templateOptions = getReportTemplateOptions(reportTemplates);
 
@@ -61,6 +67,7 @@ const AVOverview: React.FC = () => {
           ? await api.addAVOverview(values)
           : await api.updateAVOverview(values);
         setTemplate(template);
+        toast.success(`Data has successfully been saved.`);
       } catch {}
     },
     [api],
@@ -69,90 +76,142 @@ const AVOverview: React.FC = () => {
   return (
     <styled.AVOverview>
       <Row className="page-header">
-        <div className="title">Evening Overview Template</div>
+        <h1>Evening Media Overview</h1>
       </Row>
-      <p>
-        An Evening Overview Template provides a way to configure the default sections and stories
-        within each section.
-      </p>
-
-      <FormikForm
-        initialValues={template}
-        onSubmit={(values, { setSubmitting }) => {
-          handleSubmit(values);
-          setSubmitting(false);
-        }}
-      >
-        {({ isSubmitting, values, setFieldValue }) => (
+      <Tabs
+        tabs={
           <>
-            <Row className="buttons">
-              <Select
-                label="Template Type"
-                name="templateType"
-                options={templateTypeOptions}
-                value={templateTypeOptions.find((o) => o.value === templateType) ?? ''}
-                onChange={(newValue: any) => {
-                  setTemplateType(newValue.value);
-                }}
-                isClearable={false}
-                width="20ch"
-              />
-              <FormikSelect
-                label="Report Template"
-                name="templateId"
-                options={templateOptions}
-                value={templateOptions.find((o) => o.value === values.templateId) ?? ''}
-                isClearable={false}
-              />
-              <Button
-                onClick={() => {}}
-                variant={ButtonVariant.success}
-                type="submit"
-                disabled={!values.sections.length}
-                className="save-items"
-              >
-                Save Template
-                {isSubmitting ? (
-                  <FaSpinner className="icon spinner" />
-                ) : (
-                  <MdSave className="icon" />
-                )}
-              </Button>
-            </Row>
-            {values.sections.map((section, index) => (
-              <OverviewSection key={index} index={index} />
-            ))}
-            <Row className="buttons">
-              <Button
-                variant={ButtonVariant.action}
-                onClick={() =>
-                  setFieldValue('sections', [
-                    ...values.sections,
-                    defaultAVOverviewTemplateSection(values.templateType),
-                  ])
-                }
-              >
-                New broadcast section <MdAdd className="icon" />
-              </Button>
-              <Show visible={!!values.sections.length}>
-                <Button
-                  onClick={() => {}}
-                  variant={ButtonVariant.success}
-                  type="submit"
-                  className="save-items"
-                >
-                  Save Template
-                  {isSubmitting ? (
-                    <FaSpinner className="icon spinner" />
-                  ) : (
-                    <MdSave className="icon" />
-                  )}
-                </Button>
-              </Show>
-            </Row>
+            <Tab
+              label="Report"
+              onClick={() => {
+                setActive('report');
+              }}
+              active={active === 'report'}
+            />
+            <Tab
+              label="Subscribers"
+              onClick={() => {
+                setActive('subscribers');
+              }}
+              active={active === 'subscribers'}
+            />
           </>
-        )}
-      </FormikForm>
+        }
+      >
+        <Show visible={active === 'report'}>
+          <Row className="page-header">
+            <div className="title">Evening Media Overview Template</div>
+          </Row>
+          <p>
+            An Evening Overview Template provides a way to configure the default sections and
+            stories within each section.
+          </p>
+          <FormikForm
+            initialValues={template}
+            onSubmit={(values, { setSubmitting }) => {
+              handleSubmit(values);
+              setSubmitting(false);
+            }}
+          >
+            {({ isSubmitting, values, setFieldValue }) => (
+              <>
+                <Row className="buttons">
+                  <Select
+                    label="Template Type"
+                    name="templateType"
+                    options={templateTypeOptions}
+                    value={templateTypeOptions.find((o) => o.value === templateType) ?? ''}
+                    onChange={(newValue: any) => {
+                      setTemplateType(newValue.value);
+                    }}
+                    isClearable={false}
+                    width="20ch"
+                  />
+                  <FormikSelect
+                    label="Report Template"
+                    name="templateId"
+                    options={templateOptions}
+                    value={templateOptions.find((o) => o.value === values.templateId) ?? ''}
+                    isClearable={false}
+                  />
+                  <Button
+                    onClick={() => {}}
+                    variant={ButtonVariant.success}
+                    type="submit"
+                    disabled={!values.sections.length}
+                    className="save-items"
+                  >
+                    Save Template
+                    {isSubmitting ? (
+                      <FaSpinner className="icon spinner" />
+                    ) : (
+                      <MdSave className="icon" />
+                    )}
+                  </Button>
+                </Row>
+                {values.sections.map((section, index) => (
+                  <OverviewSection key={index} index={index} />
+                ))}
+                <Row className="buttons">
+                  <Button
+                    variant={ButtonVariant.action}
+                    onClick={() =>
+                      setFieldValue('sections', [
+                        ...values.sections,
+                        defaultAVOverviewTemplateSection(values.templateType),
+                      ])
+                    }
+                  >
+                    New broadcast section <MdAdd className="icon" />
+                  </Button>
+                  <Show visible={!!values.sections.length}>
+                    <Button
+                      onClick={() => {}}
+                      variant={ButtonVariant.success}
+                      type="submit"
+                      className="save-items"
+                    >
+                      Save Template
+                      {isSubmitting ? (
+                        <FaSpinner className="icon spinner" />
+                      ) : (
+                        <MdSave className="icon" />
+                      )}
+                    </Button>
+                  </Show>
+                </Row>
+              </>
+            )}
+          </FormikForm>
+        </Show>
+        <Show visible={active === 'subscribers'}>
+          <Row className="page-header">
+            <div className="title">Update Subscribers</div>
+          </Row>
+          <FormikForm
+            initialValues={template}
+            onSubmit={(values, { setSubmitting }) => {
+              handleSubmit(values);
+              setSubmitting(false);
+            }}
+          >
+            {({ isSubmitting }) => (
+              <>
+                <p>
+                  Select users to be subscribed to this template:{' '}
+                  <b>Evening Overview - {templateType}</b>
+                </p>
+                <OverviewSubscribers />
+                <Row justifyContent="center" className="form-inputs">
+                  <Button type="submit" disabled={isSubmitting}>
+                    Save
+                  </Button>
+                </Row>
+              </>
+            )}
+          </FormikForm>
+        </Show>
+      </Tabs>
     </styled.AVOverview>
   );
 };
---
+++ app/editor/src/features/admin/av-overviews/OverviewSubscribers.tsx
@@ -0,0 +1,64 @@
+import { useFormikContext } from 'formik';
+import React from 'react';
+import { useUsers } from 'store/hooks/admin';
+import { FlexboxTable, ITableInternal, ITablePage, ITableSort, IUserModel } from 'tno-core';
+
+import { subscriberColumns } from './constants';
+import { IAVOverviewTemplateForm } from './interfaces';
+import { ListFilter } from './ListFilter';
+
+/**
+ * The page used to view and edit reports.
+ * @returns Component.
+ */
+export const OverviewSubscribers: React.FC = () => {
+  const { values, setFieldValue } = useFormikContext<IAVOverviewTemplateForm>();
+  const [{ users }, { findUsers }] = useUsers();
+
+  React.useEffect(() => {
+    async function getUsers() {
+      await findUsers({ page: 1, quantity: users.quantity });
+    }
+    getUsers();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handlePageChange = React.useCallback(
+    async (page: ITablePage, table: ITableInternal<IUserModel>) => {
+      await findUsers({ page: page.pageIndex + 1, quantity: page.pageSize });
+    },
+    [findUsers],
+  );
+
+  const handleSortChange = React.useCallback(
+    async (sort: ITableSort<IUserModel>[], table: ITableInternal<IUserModel>) => {
+      const sorts = sort
+        .filter((s) => s.isSorted)
+        .map((s) => `${s.id}${s.isSortedDesc ? ' desc' : ''}`);
+      await findUsers({ page: 1, quantity: users.quantity, sort: sorts });
+    },
+    [findUsers, users.quantity],
+  );
+
+  return (
+    <>
+      <ListFilter
+        onSearch={async (value: string) => {
+          await findUsers({ page: 1, quantity: users.quantity, keyword: value });
+        }}
+      />
+      <FlexboxTable
+        rowId="id"
+        columns={subscriberColumns(values, setFieldValue)}
+        data={users.items}
+        manualPaging
+        pageIndex={users.page - 1}
+        pageSize={users.quantity}
+        pageCount={Math.ceil(users.total / users.quantity)}
+        onPageChange={handlePageChange}
+        onSortChange={handleSortChange}
+        showSort
+      />
+    </>
+  );
+};
