b4b62af6f0d7281ef64e667046d55ca3679a0dfb
Cello Liu <liu.cello@gmail.com>
2023-10-19 12:05:04 -0700

    TNO-1503 updates

--- libs/net/dal/Migrations/1.0.77/Up/PostUp/00-Content.sql
+++ libs/net/dal/Migrations/1.0.77/Up/PostUp/00-Content.sql
@@ -1,8 +1,8 @@
 DO $$
 BEGIN
 
-UPDATE public.content SET summary = '' WHERE summary = '[TBD]';
+UPDATE public.content SET summary = '' WHERE summary IN ('[TBD]', '<p>TBD</p>');
 
-UPDATE public.content SET headline = '' WHERE headline = '[TBD]';
+UPDATE public.content SET headline = '' WHERE headline IN ('[TBD]', '<p>TBD</p>');
 
 END $$;
