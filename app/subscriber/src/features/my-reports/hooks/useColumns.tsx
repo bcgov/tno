dd34a07e8aa1371ff068aff2d00a384848e8d50f
jdtoombs <jdtoombs@gmail.com>
2023-10-25 16:57:42 -0700

    lint

--- app/subscriber/src/features/my-reports/hooks/useColumns.tsx
+++ app/subscriber/src/features/my-reports/hooks/useColumns.tsx
@@ -1,4 +1,4 @@
-import { FaEdit, FaEye, FaFileAlt, FaTrash } from 'react-icons/fa';
+import { FaEdit, FaFileAlt, FaTrash } from 'react-icons/fa';
 import { FaChartPie, FaGear } from 'react-icons/fa6';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
