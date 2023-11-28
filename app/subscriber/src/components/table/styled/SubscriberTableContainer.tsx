43605b8d5b55250ffa5654bd05327615f1c700c4
Jonny Toombs <jdtoombs@gmail.com>
2023-11-27 20:18:18 -0800

    TNO-2022: Update search list view styling (#1279)

---
+++ app/subscriber/src/components/table/styled/SubscriberTableContainer.tsx
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+import { Col } from 'tno-core';
+
+export const SubscriberTableContainer = styled(Col)`
+  .table {
+    width: 100%;
+    overflow: hidden;
+
+    svg {
+      margin-right: 0.5rem;
+      height: 27px;
+      min-height: 27px;
+      max-height: 27px;
+      flex-shrink: 0;
+      &:hover {
+        cursor: pointer;
+        transform: scale(1.1);
+      }
+    }
+    svg:not(.darker-icon) {
+      color: ${(props) => props.theme.css.iconPrimaryColor};
+    }
+
+    .darker-icon {
+      color: ${(props) => props.theme.css.btnBkPrimary};
+    }
+
+    .row {
+      font-size: 1em;
+      font-weight: bold;
+      color: ${(props) => props.theme.css.btnBkPrimary};
+      &:hover {
+        cursor: pointer;
+      }
+    }
+    .header {
+      color: ${(props) => props.theme.css.fPrimaryColor};
+      font-size: 1em;
+      border-top: none;
+      border-bottom: 1px solid ${(props) => props.theme.css.linePrimaryColor};
+    }
+  }
+`;
