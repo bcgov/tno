810daaf020f11a2224c5561ece214ee8d939ad85
jdtoombs <jdtoombs@gmail.com>
2023-11-29 15:35:40 -0800

    TNO-1971: Elastic search help text

--- app/subscriber/src/components/tooltip-menu/styled/TooltipMenu.tsx
+++ app/subscriber/src/components/tooltip-menu/styled/TooltipMenu.tsx
@@ -5,7 +5,9 @@ export const TooltipMenu = styled(Tooltip)`
   z-index: 999;
   border-radius: 0.25em;
   border: 1px solid ${({ theme }) => theme.linePrimaryColor};
+  background-color: ${({ theme }) => theme.bgPrimaryColor};
   color: ${({ theme }) => theme.fPrimaryColor};
+  opacity: 1;
   .option&:hover {
     text-decoration: underline;
   }
