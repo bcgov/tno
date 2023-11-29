5984d5680e16b4b9ef2ce64744e6ee676940d725
jdtoombs <jdtoombs@gmail.com>
2023-11-28 21:32:37 -0800

    basic search

---
+++ app/subscriber/src/components/basic-search/BasicSearch.tsx
@@ -0,0 +1,64 @@
+import { filterFormat } from 'features/search-page/utils';
+import { FaPlay, FaSearch } from 'react-icons/fa';
+import { useNavigate } from 'react-router';
+import { useContent } from 'store/hooks';
+import { Button, Row, Text, toQueryString } from 'tno-core';
+
+import * as styled from './styled';
+
+/** Basic search functionality (just search term), and an option to get to the advanced filter */
+export const BasicSearch: React.FC = () => {
+  const [
+    {
+      search: { filter },
+    },
+    { storeSearchFilter },
+  ] = useContent();
+  const navigate = useNavigate();
+
+  const handleSearch = async () => {
+    navigate(`/search?${toQueryString(filterFormat(filter))}`);
+  };
+
+  const enterPressed = (e: React.KeyboardEvent) => {
+    if (e.key === 'Enter') {
+      handleSearch();
+    }
+  };
+
+  return (
+    <styled.BasicSearch>
+      <label>SEARCH FOR: </label>
+      <Row className="icon-search">
+        <FaSearch onClick={() => handleSearch()} className="search-icon" />
+        <Text
+          className="search-input"
+          onKeyDown={enterPressed}
+          name="search"
+          onChange={(e) => {
+            storeSearchFilter({ ...filter, search: e.target.value });
+          }}
+        />
+      </Row>
+      <Text
+        className="search-mobile"
+        onKeyDown={enterPressed}
+        name="search-mobile"
+        onChange={(e) => {
+          storeSearchFilter({ ...filter, search: e.target.value });
+        }}
+      />
+
+      <Button
+        onClick={() => {
+          handleSearch();
+        }}
+        className="search-button"
+      >
+        Search
+        <FaPlay />
+      </Button>
+      <p onClick={() => navigate('/search')}>GO ADVANCED</p>
+    </styled.BasicSearch>
+  );
+};
