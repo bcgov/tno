6d92cc58c81bd85d9e4436470632eed622d7c7a1
kylgarmor <kyle@nexusfusion.com>
2023-11-30 08:16:10 -0800

    feat: #tno-1937 - feature complete - fix enum details

--- libs/net/entities/AdminConfigurableSettingNames.cs
+++ libs/net/entities/AdminConfigurableSettingNames.cs
@@ -3,12 +3,12 @@ using TNO.Core.Json;
 namespace TNO.Entities;
 
 /// <summary>
-/// Provides report status.
+/// Used to hold names of settings that can be configured in th Editor > Settings UI.
 /// </summary>
 public enum AdminConfigurableSettingNames
 {
     /// <summary>
-    /// Report is pending to be sent.
+    /// Email for the admin responsible for processing report subscription requests.
     /// </summary>
     [EnumValue("ProductSubscriptionManagerEmail")]
     ProductSubscriptionManagerEmail,
