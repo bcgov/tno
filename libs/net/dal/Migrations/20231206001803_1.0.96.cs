16721faa551b5fd5abc9a7b83d2c8048698d93b2
kylgarmor <kyle@nexusfusion.com>
2023-12-05 16:28:04 -0800

    fix: #tno-2049 - fixed relationships - changed Content and Source table relationships to User table - need to allow owner_id to be setNull on delete User

---
+++ libs/net/dal/Migrations/20231206001803_1.0.96.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using TNO.DAL;
+
+#nullable disable
+
+namespace TNO.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class _1096 : SeedMigration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            PreUp(migrationBuilder);
+            migrationBuilder.DropForeignKey(
+                name: "FK_content_user_owner_id",
+                table: "content");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_source_user_owner_id",
+                table: "source");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_content_user_owner_id",
+                table: "content",
+                column: "owner_id",
+                principalTable: "user",
+                principalColumn: "id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_source_user_owner_id",
+                table: "source",
+                column: "owner_id",
+                principalTable: "user",
+                principalColumn: "id",
+                onDelete: ReferentialAction.SetNull);
+            PostUp(migrationBuilder);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            PreDown(migrationBuilder);
+            migrationBuilder.DropForeignKey(
+                name: "FK_content_user_owner_id",
+                table: "content");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_source_user_owner_id",
+                table: "source");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_content_user_owner_id",
+                table: "content",
+                column: "owner_id",
+                principalTable: "user",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_source_user_owner_id",
+                table: "source",
+                column: "owner_id",
+                principalTable: "user",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+            PostDown(migrationBuilder);
+        }
+    }
+}
