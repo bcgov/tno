238cd3f6785ddda38870ffd2111f41a96475b7e3
kylgarmor <kyle@nexusfusion.com>
2023-12-07 15:15:01 -0800

    fix: #tno-1708 - fix for weird inheritance issue

--- libs/net/ches/Models/IEmail.cs
+++ libs/net/ches/Models/IEmail.cs
@@ -16,15 +16,7 @@ namespace TNO.Ches.Models
         /// get/set - Email addresses to go in the To: field
         /// </summary>
         IEnumerable<string> To { get; set; }
-
-        /// <summary>
-        /// get/set - Email addresses to go in the CC: field
-        /// </summary>
         IEnumerable<string> Cc { get; set; }
-
-        /// <summary>
-        /// get/set - Email addresses to go in the BCC: field
-        /// </summary>
         IEnumerable<string> Bcc { get; set; }
 
         /// <summary>
