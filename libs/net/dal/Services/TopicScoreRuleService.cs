dd8d84c634919225c1ebd919c318b58ffcb21f14
kylgarmor <63015960+kylgarmor@users.noreply.github.com>
2023-12-28 14:24:28 -0800

    feat: #tno-2137 - changed Update method to only Update changed entities (#1354)
    - introduced an Equality operator on TopicScoreRule to check for changes

--- libs/net/dal/Services/TopicScoreRuleService.cs
+++ libs/net/dal/Services/TopicScoreRuleService.cs
@@ -1,6 +1,7 @@
 using System.Security.Claims;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using TNO.Core.Exceptions;
 using TNO.Entities;
 
 namespace TNO.DAL.Services;
@@ -8,15 +9,34 @@ namespace TNO.DAL.Services;
 public class TopicScoreRuleService : BaseService<TopicScoreRule, int>, ITopicScoreRuleService
 {
     #region Properties
+    ILogger<TopicScoreRuleService> logger;
     #endregion
 
     #region Constructors
     public TopicScoreRuleService(TNOContext dbContext, ClaimsPrincipal principal, IServiceProvider serviceProvider, ILogger<TopicScoreRuleService> logger) : base(dbContext, principal, serviceProvider, logger)
     {
+        this.logger = logger;
     }
     #endregion
 
     #region Methods
+    /// <summary>
+    /// Update the specified 'entity' in the context so that it can be updated in the database.
+    /// This operation does not commit the transaction to the database.
+    /// </summary>
+    /// <param name="entity"></param>
+    /// <returns></returns>
+    public override TopicScoreRule Update(TopicScoreRule entity) {
+        var original = FindById(entity.Id) ?? throw new NoContentException("Entity does not exist");
+
+        if (!original.Equals(entity)) {
+            logger.LogDebug($"Entity {entity.Id} unchanged, no need to update.");
+            return base.Update(entity);
+        }
+        else
+            return entity;
+    }
+
     public IEnumerable<TopicScoreRule> FindAll()
     {
         return this.Context.TopicScoreRules
