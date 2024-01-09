1e5dbb601d89df1761be71bb19612e4a532b85d0
Felipe Barreta <fbarreta@gmail.com>
2024-01-08 17:49:59 -0800

    TNO-2113 Share to an email

--- app/subscriber/src/components/share-menu/styled/ShareMenu.tsx
+++ app/subscriber/src/components/share-menu/styled/ShareMenu.tsx
@@ -23,4 +23,30 @@ export const ShareMenu = styled.div`
     color: inherit;
     text-decoration: none;
   }
+
+  input {
+    font-size: 1em;
+    border: none;
+    border-bottom: 0.1rem solid;
+    border-radius: 0;
+    padding: 0;
+    &:focus {
+      box-shadow: none;
+      outline: none;
+    }
+  }
+  .share-email {
+    display: flex;
+    background-color: transparent;
+    color: ${(props) => props.theme.css.btnBkPrimary};
+    &:hover {
+      cursor: pointer;
+      color: ${(props) => props.theme.css.btnBkPrimary};
+    }
+
+    border: 0.1em solid ${(props) => props.theme.css.btnBkPrimary};
+    height: 1.5em;
+    margin-left: 0;
+    min-width: fit-content;
+  }
 `;
