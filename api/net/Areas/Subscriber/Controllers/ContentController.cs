d91ba5ee2e3a937f9af4ba136012fc0801b5d133
Felipe Barreta <fbarreta@gmail.com>
2024-01-16 12:33:56 -0800

    TNO-2113 Exception change

--- api/net/Areas/Subscriber/Controllers/ContentController.cs
+++ api/net/Areas/Subscriber/Controllers/ContentController.cs
@@ -346,7 +346,7 @@ public class ContentController : ControllerBase
 
         var username = User.GetUsername() ?? throw new NotAuthorizedException("Username is missing");
         var user = _userService.FindByUsername(username) ?? throw new NotAuthorizedException("User does not exist");
-        var subscriber = _userService.FindByEmail(email).FirstOrDefault() ?? throw new NotAuthorizedException("Subscriber does not exist");
+        var subscriber = _userService.FindByEmail(email).FirstOrDefault() ?? throw new InvalidOperationException("Subscriber does not exist");
 
         var request = new NotificationRequestModel(NotificationDestination.NotificationService, new { })
         {
