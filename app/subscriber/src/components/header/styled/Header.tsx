cde89f50c407466cba44a59a744e04108cd5978b
Jonny Toombs <jdtoombs@gmail.com>
2024-01-19 12:04:42 -0800

    small css things (#1435)

--- app/subscriber/src/components/header/styled/Header.tsx
+++ app/subscriber/src/components/header/styled/Header.tsx
@@ -5,7 +5,6 @@ export const Header = styled.div`
   flex-direction: row;
   gap: 1rem;
   padding: 0.75rem;
-  border-bottom: solid 1px ${(props) => props.theme.css.primaryLineColor};
 
   .logo-container {
     width: 15em;
