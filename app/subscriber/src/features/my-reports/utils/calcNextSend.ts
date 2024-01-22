1e460af42e42ef4b5aee100ef767ada18211532f
Mihai <mihailistov@gmail.com>
2024-01-22 09:26:18 -0800

    TNO-2192: Fix issue calculating next report send date (#1443)
    * TNO-2192: Fix issue calculating next report send date
    
    * TNO-2192: Fix bug with next send on date being in past
    
    * fixup! TNO-2192: Fix bug with next send on date being in past

--- app/subscriber/src/features/my-reports/utils/calcNextSend.ts
+++ app/subscriber/src/features/my-reports/utils/calcNextSend.ts
@@ -17,17 +17,15 @@ interface ISchedule {
 }
 
 function getNextReportSend(report: IReportModel): string {
-  // Set constants with the last sent info. If it's null consider the actual date
-  const sentOn: string | Date | undefined = report.instances.length
-    ? report.instances[0].sentOn
-    : undefined;
-
-  const lastSent: Date = sentOn === undefined ? new Date() : new Date(sentOn);
-
-  const lastWeekDay = lastSent.toLocaleDateString('en-US', { weekday: 'long' });
-
-  const lastTimeSent = lastSent.toLocaleTimeString('en-US', {
-    timeZone: 'UTC',
+  const currentTime: Date = new Date();
+  const currentWeekDay = currentTime.toLocaleDateString('en-US', {
+    timeZone: 'America/Vancouver',
+    weekday: 'long',
+  });
+  const currentTimeOfDay = currentTime.toLocaleTimeString('en-US', {
+    // TODO: Store User's preferred timezone in profile/settings to use for coverting/storing
+    // Server currently compares time in server local time, which is PST. Hence, use PST.
+    timeZone: 'America/Vancouver',
     hour12: false,
     hour: '2-digit',
     minute: '2-digit',
@@ -53,32 +51,32 @@ function getNextReportSend(report: IReportModel): string {
   sends.sort((a, b) => a.weekDayNum - b.weekDayNum || a.time.localeCompare(b.time));
 
   // Run through the available schedule day, and add the proper to the variable
-  // Calculate the day difference between the last send/future send
+  // Calculate the day difference between now and next send
   let nextSend: ISchedule | null = null;
   let daysToAdd: number = 0;
   for (let i = 0; i < sends.length; i++) {
-    if (sends[i].weekDayNum > WeekDays[lastWeekDay]) {
-      daysToAdd = sends[i].weekDayNum - WeekDays[lastWeekDay];
+    if (sends[i].weekDayNum > WeekDays[currentWeekDay]) {
+      daysToAdd = sends[i].weekDayNum - WeekDays[currentWeekDay];
       nextSend = sends[i];
       break;
     }
 
-    if (sends[i].weekDayNum === WeekDays[lastWeekDay]) {
-      if (sends[i].time > lastTimeSent) {
-        daysToAdd = sends[i].weekDayNum - WeekDays[lastWeekDay];
+    if (sends[i].weekDayNum === WeekDays[currentWeekDay]) {
+      if (sends[i].time > currentTimeOfDay) {
+        daysToAdd = sends[i].weekDayNum - WeekDays[currentWeekDay];
         nextSend = sends[i];
         break;
       }
     }
     if (nextSend === null) {
       // Advance one week and set to the first available day
-      daysToAdd = sends[0].weekDayNum + 7 - WeekDays[lastWeekDay];
+      daysToAdd = sends[0].weekDayNum + 7 - WeekDays[currentWeekDay];
       nextSend = sends[0];
     }
   }
 
   // Add the day difference to the last sent date and format correctly to display
-  const nextSendDate = new Date(lastSent.setDate(lastSent.getDate() + daysToAdd));
+  const nextSendDate = new Date(currentTime.setDate(currentTime.getDate() + daysToAdd));
   const year = nextSendDate.toLocaleDateString('en-US', { year: 'numeric' });
   const month = nextSendDate.toLocaleDateString('en-US', { month: '2-digit' });
   const day = nextSendDate.toLocaleDateString('en-US', { day: '2-digit' });
