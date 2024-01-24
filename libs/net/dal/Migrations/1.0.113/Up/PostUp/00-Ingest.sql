776626df1528ee32fbecbe0d886262d471d17fa7
kylgarmor <63015960+kylgarmor@users.noreply.github.com>
2024-01-24 09:21:44 -0800

    fix: #tno-2228 - added additional migration to ensure setting is initialized (#1454)

---
+++ libs/net/dal/Migrations/1.0.113/Up/PostUp/00-Ingest.sql
@@ -0,0 +1,20 @@
+DO $$
+
+BEGIN
+
+UPDATE public.ingest i
+set configuration = jsonb_set(configuration, '{importMigrationType}', '"Historic"')
+where i.source_id = (select id from public.source where code = 'TNO')
+and name like '% - HISTORIC';
+
+UPDATE public.ingest i
+set configuration = jsonb_set(configuration, '{importMigrationType}', '"Recent"')
+where i.source_id = (select id from public.source where code = 'TNO')
+and name like '% - RECENT';
+
+UPDATE public.ingest i
+set configuration = jsonb_set(configuration, '{importMigrationType}', '"RecentlyPublished"')
+where i.source_id = (select id from public.source where code = 'TNO')
+and name like '% - RECENTLY-PUBLISHED';
+
+END $$;
