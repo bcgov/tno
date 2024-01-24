6f6d39d80469cf7783c50990f5ac48c149fadf1c
root <jdtoombs@gmail.com>
2024-01-24 15:25:27 -0800

    remove enum

--- app/subscriber/src/features/filter-media/FilterMediaLanding.tsx
+++ app/subscriber/src/features/filter-media/FilterMediaLanding.tsx
@@ -1,83 +1,40 @@
 import { useFilterOptions } from 'components/navbar';
 import { PageSection } from 'components/section';
+import {
+  ISubMediaGroupItem,
+  SubMediaGroups,
+} from 'features/search-page/components/advanced-search/constants';
 import React from 'react';
-import { useContent, useLookup } from 'store/hooks';
+import { useContent } from 'store/hooks';
 import { Col, ISourceModel, Row, Show } from 'tno-core';
 
-import { MediaFilterTypes } from './constants';
 import { FilterMedia } from './FilterMedia';
 import * as styled from './styled';
 import { alphabetArray } from './utils';
 
 /** The FilterMediaLanding Consists of the filter to narrow down the results of media, as well as the results. It is returned in a two column layout*/
 export const FilterMediaLanding: React.FC = () => {
-  const { dailyPrint, weeklyPrint, cpWire, talkRadio, onlinePrint, television, newsRadio } =
-    useFilterOptions();
-  const [{ mediaTypes }] = useLookup();
-  console.log(mediaTypes);
+  const {
+    dailyPrint,
+    weeklyPrint,
+    cpWire,
+    talkRadio,
+    onlinePrint,
+    television,
+    newsRadio,
+    sources,
+  } = useFilterOptions();
   const [
     {
       mediaType: { filter },
     },
     { storeMediaTypeFilter: storeFilter },
   ] = useContent();
-  const [activeFilter, setActiveFilter] = React.useState<MediaFilterTypes>(
-    MediaFilterTypes.DAILY_PRINT,
-  );
+  const [activeFilter, setActiveFilter] = React.useState<ISubMediaGroupItem>();
 
   const [activeLetter, setActiveLetter] = React.useState<string>('All');
   const [narrowedOptions, setNarrowedOptions] = React.useState<ISourceModel[]>([]);
   const [activeSource, setActiveSource] = React.useState<ISourceModel | null>(null);
-
-  const labels = Object.values(MediaFilterTypes);
-  const determineOptions = () => {
-    switch (activeFilter) {
-      case MediaFilterTypes.DAILY_PRINT:
-        return dailyPrint;
-      case MediaFilterTypes.WEEKLY_PRINT:
-        return weeklyPrint;
-      case MediaFilterTypes.INTERNET:
-        return onlinePrint;
-      case MediaFilterTypes.TV:
-        return television;
-      case MediaFilterTypes.TALK_RADIO:
-        return talkRadio;
-      case MediaFilterTypes.RADIO_NEWS:
-        return newsRadio;
-      case MediaFilterTypes.CP_NEWS:
-        return cpWire;
-      default:
-        return [];
-    }
-  };
-
-  const determineShowAll = () => {
-    switch (activeFilter) {
-      case MediaFilterTypes.DAILY_PRINT:
-        storeFilter({ ...filter, sourceIds: dailyPrint.map((opt) => opt.id) });
-        break;
-      case MediaFilterTypes.WEEKLY_PRINT:
-        storeFilter({ ...filter, sourceIds: weeklyPrint.map((opt) => opt.id) });
-        break;
-      case MediaFilterTypes.INTERNET:
-        storeFilter({ ...filter, sourceIds: onlinePrint.map((opt) => opt.id) });
-        break;
-      case MediaFilterTypes.TV:
-        storeFilter({ ...filter, sourceIds: television.map((opt) => opt.id) });
-        break;
-      case MediaFilterTypes.TALK_RADIO:
-        storeFilter({ ...filter, sourceIds: talkRadio.map((opt) => opt.id) });
-        break;
-      case MediaFilterTypes.RADIO_NEWS:
-        storeFilter({ ...filter, sourceIds: newsRadio.map((opt) => opt.id) });
-        break;
-      case MediaFilterTypes.CP_NEWS:
-        storeFilter({ ...filter, sourceIds: cpWire.map((opt) => opt.id) });
-        break;
-      default:
-        break;
-    }
-  };
 
   React.useEffect(() => {
     if (dailyPrint && !narrowedOptions.length) setNarrowedOptions(dailyPrint);
@@ -85,22 +42,16 @@ export const FilterMediaLanding: React.FC = () => {
 
   React.useEffect(() => {
     if (activeLetter && activeLetter !== 'All') {
-      const options = determineOptions();
-      setNarrowedOptions(options.filter((opt) => opt.name.startsWith(activeLetter)));
+      setNarrowedOptions(
+        activeFilter?.options.filter((opt) => opt.name.startsWith(activeLetter)) ?? [],
+      );
     }
     if (activeLetter === 'All') {
-      setNarrowedOptions(determineOptions());
+      setNarrowedOptions(activeFilter?.options ?? []);
     }
     // only want to fire when filters change
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [activeLetter, activeFilter]);
-
-  /** When the active filter changes, we want to show all options in that category before the user narrows it down*/
-  React.useEffect(() => {
-    determineShowAll();
-    // only want to fire when filters change
-    //eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [activeFilter]);
 
   return (
     <styled.FilterMediaLanding>
@@ -109,25 +60,41 @@ export const FilterMediaLanding: React.FC = () => {
           {/* TODO: Move into reusable component, this type of filter is only used on this page currently*/}
           <Row>
             <Col className="media-filter">
-              {labels.map((label) => (
+              {SubMediaGroups(
+                dailyPrint,
+                weeklyPrint,
+                cpWire,
+                talkRadio,
+                onlinePrint,
+                newsRadio,
+                television,
+                sources,
+              ).map((mediaGroup) => (
                 <div
-                  key={`${label}`}
+                  key={`${mediaGroup.key}`}
                   onClick={() => {
-                    if (label === MediaFilterTypes.WEEKLY_PRINT) setActiveLetter('A');
-                    if (label === MediaFilterTypes.INTERNET) setActiveLetter('B');
-                    setActiveFilter(label);
+                    if (mediaGroup.label === 'Weekly Print' || mediaGroup.label === 'All')
+                      setActiveLetter('A');
+                    if (mediaGroup.label === 'Online') setActiveLetter('B');
+                    setActiveFilter(mediaGroup);
                   }}
-                  className={`${activeFilter === label ? 'active' : 'inactive'} option`}
+                  className={`${
+                    activeFilter?.label === mediaGroup.label ||
+                    (!activeFilter && mediaGroup.label === 'Daily Print')
+                      ? 'active'
+                      : 'inactive'
+                  } option`}
                 >
-                  {label}
+                  {mediaGroup.label}
                 </div>
               ))}
             </Col>
             <Col className="narrowed-options">
               <Show
                 visible={
-                  activeFilter === MediaFilterTypes.INTERNET ||
-                  activeFilter === MediaFilterTypes.WEEKLY_PRINT
+                  activeFilter?.label === 'Online' ||
+                  activeFilter?.label === 'Weekly Print' ||
+                  activeFilter?.label === 'All'
                 }
               >
                 <Row className="alpha-filter">
@@ -147,7 +114,7 @@ export const FilterMediaLanding: React.FC = () => {
               <div
                 onClick={() => {
                   setActiveSource(null);
-                  determineShowAll();
+                  setNarrowedOptions(activeFilter?.options ?? []);
                 }}
                 className="show-all"
               >
--- app/subscriber/src/features/filter-media/constants/MediaFilterTypes.ts
+++
@@ -1,9 +0,0 @@
-export enum MediaFilterTypes {
-  DAILY_PRINT = 'Daily Print',
-  WEEKLY_PRINT = 'Weekly Print',
-  RADIO_NEWS = 'Radio News',
-  TALK_RADIO = 'Talk Radio',
-  TV = 'Television',
-  INTERNET = 'Internet',
-  CP_NEWS = 'CP News',
-}
