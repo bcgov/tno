b9edfcf5a2b6e6bd116b5801f6439742282abe4d
Jonny Toombs <jdtoombs@gmail.com>
2024-02-02 09:41:33 -0800

    fix dev bug when no tone value (#1483)

--- app/subscriber/src/components/content-list/utils/determineToneIcon.tsx
+++ app/subscriber/src/components/content-list/utils/determineToneIcon.tsx
@@ -1,8 +1,8 @@
 import { FaFrown, FaMeh, FaSmile } from 'react-icons/fa';
 import { IContentTonePoolModel } from 'tno-core';
 
-export const determineToneIcon = (tone: IContentTonePoolModel) => {
-  const value = tone.value;
+export const determineToneIcon = (tone?: IContentTonePoolModel) => {
+  const value = tone?.value ?? 0;
   if (value > 0) return <FaSmile className="positive tone-icon" />;
   if (value < 0) return <FaFrown className="negative tone-icon" />;
   if (value === 0) return <FaMeh className="neutral tone-icon" />;
