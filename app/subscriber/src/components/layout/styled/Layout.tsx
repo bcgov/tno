a18a84353bb52e0019ebb3ffc9f1cd9e2a200e98
jdtoombs <jdtoombs@gmail.com>
2024-02-05 15:16:27 -0800

    fix login height

--- app/subscriber/src/components/layout/styled/Layout.tsx
+++ app/subscriber/src/components/layout/styled/Layout.tsx
@@ -3,32 +3,34 @@ import styled from 'styled-components';
 import { ILayoutProps } from '..';
 
 export const Layout = styled.div<ILayoutProps>`
-  main {
-    overflow: clip auto;
-    height: calc(100dvh - 4.75rem);
-  }
-  .header {
-    grid-area: header;
-  }
+  &:not(.unauth) {
+    main {
+      overflow: clip auto;
+      height: calc(100dvh - 4.75rem);
+    }
+    .header {
+      grid-area: header;
+    }
 
-  .nav-bar {
-    grid-area: nav-bar;
-  }
+    .nav-bar {
+      grid-area: nav-bar;
+    }
 
-  .contents-container {
-    grid-area: content;
-  }
+    .contents-container {
+      grid-area: content;
+    }
 
-  .grid-container {
-    height: 100dvh;
-    overflow: clip;
-    display: grid;
-    transition: 300ms;
-    background-color: ${(props) => props.theme.css.bkMain};
-    grid-template-areas:
-      'header header'
-      'nav-bar content';
-    grid-auto-columns: max-content 8fr;
-    grid-auto-rows: auto;
+    .grid-container {
+      height: 100dvh;
+      overflow: clip;
+      display: grid;
+      transition: 300ms;
+      background-color: ${(props) => props.theme.css.bkMain};
+      grid-template-areas:
+        'header header'
+        'nav-bar content';
+      grid-auto-columns: max-content 8fr;
+      grid-auto-rows: auto;
+    }
   }
 `;
