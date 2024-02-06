2749dc2e31c3dafadb7f9ec3006c95231768118e
Felipe Barreta <fbarreta@gmail.com>
2024-02-06 10:40:21 -0800

    TNO-2001 Sort function on group by

--- app/subscriber/src/components/content-list/utils/groupContent.ts
+++ app/subscriber/src/components/content-list/utils/groupContent.ts
@@ -4,25 +4,59 @@ import moment from 'moment';
 import { IGroupByState } from '../interfaces';
 
 export const groupContent = (groupBy: IGroupByState, content: IContentSearchResult[]) => {
-  const grouped = content.reduce((acc, item) => {
-    if (!item?.source?.name) return acc; // skip if no source
-    let key = item.source?.name; // default to source
-    switch (groupBy) {
-      case 'time':
-        const date = new Date(item.publishedOn);
-        const hours = date.getHours().toString().padStart(2, '0');
-        const minutes = date.getMinutes().toString().padStart(2, '0');
-        key = `${moment(date).format('DD/MM/YY')} (${hours}:${minutes})`;
-        break;
+  const sortFunc = (key: string) => {
+    switch (key) {
+      case 'published':
+        return (a: IContentSearchResult, b: IContentSearchResult) =>
+          a.publishedOn > b.publishedOn ? 1 : -1;
       case 'source':
-        key = item.source.name;
-        break;
+        return (a: IContentSearchResult, b: IContentSearchResult) => {
+          if (a.source && b.source) {
+            return a.source.sortOrder > b.source.sortOrder ? 1 : -1;
+          }
+          return -1;
+        };
+      default:
+        return (a: IContentSearchResult, b: IContentSearchResult) =>
+          a.publishedOn > b.publishedOn ? 1 : -1;
     }
-    if (!acc[key]) {
-      acc[key] = [];
-    }
-    acc[key].push(item);
-    return acc;
-  }, {} as Record<string, IContentSearchResult[]>);
+  };
+  let firstSort = 'published';
+  let secondSort = 'source';
+
+  switch (groupBy) {
+    case 'time':
+      firstSort = 'source';
+      secondSort = 'published';
+      break;
+    case 'source':
+      firstSort = 'published';
+      secondSort = 'source';
+      break;
+  }
+
+  const grouped = content
+    .sort(sortFunc(firstSort))
+    .sort(sortFunc(secondSort))
+    .reduce((acc, item) => {
+      if (!item?.source?.name) return acc; // skip if no source
+      let key = item.source?.name; // default to source
+      switch (groupBy) {
+        case 'time':
+          const date = new Date(item.publishedOn);
+          const hours = date.getHours().toString().padStart(2, '0');
+          const minutes = date.getMinutes().toString().padStart(2, '0');
+          key = `${moment(date).format('DD/MM/YY')} (${hours}:${minutes})`;
+          break;
+        case 'source':
+          key = item.source.name;
+          break;
+      }
+      if (!acc[key]) {
+        acc[key] = [];
+      }
+      acc[key].push(item);
+      return acc;
+    }, {} as Record<string, IContentSearchResult[]>);
   return grouped;
 };
