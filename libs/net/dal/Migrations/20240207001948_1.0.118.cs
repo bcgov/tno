8dbe07383424d7cca8ebc04c4fcb19c70496d706
kylgarmor <kyle@nexusfusion.com>
2024-02-07 11:41:20 -0800

    feat: tno-2289 - split tno historic service out into own config

---
+++ libs/net/dal/Migrations/20240207001948_1.0.118.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using TNO.DAL;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TNO.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class _10118 : SeedMigration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            PreUp(migrationBuilder);
+
+            PostUp(migrationBuilder);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            PreDown(migrationBuilder);
+
+            PostDown(migrationBuilder);
+        }
+    }
+}
