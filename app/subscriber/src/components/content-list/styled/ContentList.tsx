e0bc1a460c441a449ae17a52c9dea5a60f10596b
jdtoombs <jdtoombs@gmail.com>
2024-02-07 17:38:18 -0800

    fix scroll issues on search page

--- app/subscriber/src/components/content-list/styled/ContentList.tsx
+++ app/subscriber/src/components/content-list/styled/ContentList.tsx
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 
-export const ContentList = styled.div`
+export const ContentList = styled.div<{ scrollWithin: boolean }>`
   .group-title {
     border-bottom: 1px solid ${(props) => props.theme.css.border};
   }
+  ${(props) => (props.scrollWithin ? 'overflow-y: auto;' : '')}
 `;
