5b5e54ddfe6578a8f8032fb97f8ce5f67ca61758
kylgarmor <63015960+kylgarmor@users.noreply.github.com>
2024-02-29 14:39:58 -0800

    fix: #tno-2359 - fixed filter and patched ReportingManager (#1576)
    - filter was breaking the report sending because it wasnt including the id
    - reporting manager fails when report send is triggered if the content id is invalid
    - fix issue with Not Applicable showing up in report output

--- libs/net/dal/Migrations/1.0.128/Up/PostUp/00-Filter.sql
+++ libs/net/dal/Migrations/1.0.128/Up/PostUp/00-Filter.sql
@@ -68,4 +68,83 @@ SET "query" = '{
 WHERE "name" = 'Event of the Day - Topic Type - last 28 days'
 AND owner_id = ServiceAccountId;
 
+-- update 365 aggregate day filter
+UPDATE public."filter"
+SET "query" = '{
+  "aggs": {
+    "aggTopics": {
+      "aggs": {
+        "aggTopicType": {
+          "aggs": {
+            "aggTopicName": {
+              "terms": {
+                "size": 3,
+                "field": "topics.name"
+              }
+            }
+          },
+          "terms": {
+            "field": "topics.topicType"
+          }
+        }
+      },
+      "nested": {
+        "path": "topics"
+      }
+    }
+  },
+  "size": 0,
+  "query": {
+    "bool": {
+      "must": [
+        {
+          "range": {
+            "publishedOn": {
+              "gte": "now-365d/d",
+              "time_zone": "US/Pacific"
+            }
+          }
+        },
+        {
+          "nested": {
+            "path": "topics",
+            "query": {
+              "exists": {
+                "field": "topics"
+              }
+            }
+          }
+        }
+      ],
+      "must_not": [
+        {
+          "terms": {
+            "mediaTypeId": [
+              10
+            ]
+          }
+        },
+        {
+          "nested": {
+            "path": "topics",
+            "query": {
+              "bool": {
+                "must": [
+                  {
+                    "match": {
+                      "topics.name": "Not Applicable"
+                    }
+                  }
+                ]
+              }
+            }
+          }
+        }
+      ]
+    }
+  }
+}' -- query
+WHERE "name" = 'Event of the Day - Rolling 365 day aggregate'
+AND owner_id = ServiceAccountId;
+
 END $$;
