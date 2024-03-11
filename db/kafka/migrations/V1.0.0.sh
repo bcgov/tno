50036fe995c2867ccaf7490f85af319b731d45a1
Jeremy Foster <jeremy.foster@fosol.ca>
2024-03-11 13:10:11 -0700

    Fixing setup (#1608)

--- db/kafka/migrations/V1.0.0.sh
+++ db/kafka/migrations/V1.0.0.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
-# Logging topics
-docker exec -i tno-broker bash -c "/bin/kafka-topics --create --topic logs-nlp --bootstrap-server $bootstrap --partitions $partitions --replication-factor $replication"
-docker exec -i tno-broker bash -c "/bin/kafka-topics --create --topic logs-elastic --bootstrap-server $bootstrap --partitions $partitions --replication-factor $replication"
-docker exec -i tno-broker bash -c "/bin/kafka-topics --create --topic logs-syndication --bootstrap-server $bootstrap --partitions $partitions --replication-factor $replication"
-docker exec -i tno-broker bash -c "/bin/kafka-topics --create --topic logs-audio --bootstrap-server $bootstrap --partitions $partitions --replication-factor $replication"
-docker exec -i tno-broker bash -c "/bin/kafka-topics --create --topic logs-video --bootstrap-server $bootstrap --partitions $partitions --replication-factor $replication"
-docker exec -i tno-broker bash -c "/bin/kafka-topics --create --topic logs-capture --bootstrap-server $bootstrap --partitions $partitions --replication-factor $replication"
-
-# Topics for ingesting news
-docker exec -i tno-broker bash -c "/bin/kafka-topics --create --topic news-ghi --bootstrap-server $bootstrap --partitions $partitions --replication-factor $replication"
-docker exec -i tno-broker bash -c "/bin/kafka-topics --create --topic news-hth --bootstrap-server $bootstrap --partitions $partitions --replication-factor $replication"
-
-# Topic for the results of NLP process
-docker exec -i tno-broker bash -c "/bin/kafka-topics --create --topic news-nlp --bootstrap-server $bootstrap --partitions $partitions --replication-factor $replication"
-
 # Topics for media capture
-docker exec -i tno-broker bash -c "/bin/kafka-topics --create --topic media-cbckam --bootstrap-server $bootstrap --partitions $partitions --replication-factor $replication"
+docker exec -i tno-broker bash -c "/bin/kafka-topics --create --topic hub --bootstrap-server $bootstrap --partitions $partitions --replication-factor $replication"
+docker exec -i tno-broker bash -c "/bin/kafka-topics --create --topic notify --bootstrap-server $bootstrap --partitions $partitions --replication-factor $replication"
+docker exec -i tno-broker bash -c "/bin/kafka-topics --create --topic index --bootstrap-server $bootstrap --partitions $partitions --replication-factor $replication"
+docker exec -i tno-broker bash -c "/bin/kafka-topics --create --topic reporting --bootstrap-server $bootstrap --partitions $partitions --replication-factor $replication"
+docker exec -i tno-broker bash -c "/bin/kafka-topics --create --topic transcribe --bootstrap-server $bootstrap --partitions $partitions --replication-factor $replication"
+docker exec -i tno-broker bash -c "/bin/kafka-topics --create --topic ffmpeg --bootstrap-server $bootstrap --partitions $partitions --replication-factor $replication"
+docker exec -i tno-broker bash -c "/bin/kafka-topics --create --topic event-schedule --bootstrap-server $bootstrap --partitions $partitions --replication-factor $replication"
