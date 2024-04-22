b2c959a808b0a686a02d1302d93243dd1a72a864
kylgarmor <63015960+kylgarmor@users.noreply.github.com>
2024-04-22 09:33:14 -0700

    fix: #tno-2405 - sections now use the folder as data source (#1731)
    * fix: #tno-2405 - sections now use the folder as data source
    - fixed issue with 24hr sections not using the folder as soruce of truth
    - updated template to be able to switch between aggregate or query to render data
    - updated folderservice to return topics on content
    - added more helper methods specific to Event Of The Day / Aggregates
    - added cleaner rollback

---
+++ libs/net/dal/Migrations/1.0.151/Up/PostUp/02-ReportSections-EventOfTheDay.sql
@@ -0,0 +1,34 @@
+DO $$
+DECLARE ReportId INT;
+DECLARE FilterId INT;
+DECLARE FolderId INT;
+BEGIN
+
+-- get the Report Id
+SELECT INTO ReportId id
+FROM public."report"
+where "name" = 'Event of the Day'
+and owner_id = 1;
+
+-- get the folderId
+select INTO FolderId fol.id
+from public."folder" fol
+join public."filter" fil on fol.filter_id = fil.id
+where fol."name" = 'Event of the Day'
+and fil."name" = 'Event of the Day - Folder Collector'
+and fol.owner_id = 1;
+
+-- get the filterId
+select INTO FilterId f.id  
+from public."filter" f 
+where f."name" = 'Event of the Day - 24hr Aggregate'
+and f.owner_id = 1;
+
+-- replace usage of the filter with the folder
+-- update the first two sections of the report
+update public.report_section
+set folder_id = FolderId, filter_id = null
+where (sort_order = 0 or sort_order = 1)
+and report_id = ReportId;
+
+END $$;
