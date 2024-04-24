a82ceaba988eedd42a0030df0d9b529836b22068
Felipe Barreta <fbarreta@gmail.com>
2024-04-24 06:45:52 -0700

    TNO-2492 Title case if all caps and config is enabled (#1739)

--- app/editor/src/features/admin/ingests/configurations/styled/IngestConfiguration.tsx
+++ app/editor/src/features/admin/ingests/configurations/styled/IngestConfiguration.tsx
@@ -1,4 +1,8 @@
 import styled from 'styled-components';
 import { Section } from 'tno-core/dist/components/section';
 
-export const IngestConfiguration = styled(Section)``;
+export const IngestConfiguration = styled(Section)`
+  .checkbox-label {
+    font-weight: bold;
+  }
+`;
