80c074edcc843b4cc9246d14970c03d73d2f6b18
Jacob Wang <cpcw1cpcw@gmail.com>
2024-04-28 23:47:09 -0700

    Add naturalSort utility function for list view sorting

---
+++ app/editor/src/features/content/list-view/utils/naturalSort.ts
@@ -0,0 +1,19 @@
+import { IContentSearchResult } from 'store/slices';
+
+function getPageSectionValue(row: IContentSearchResult) {
+  // gnerate page:section string, keep it in the lowercase
+  const value = `${row.original.page ? row.original.page : ''}:${
+    row.original.section ? row.original.section : ''
+  }`.toLowerCase();
+  return value;
+}
+
+export function naturalSortValue(row: IContentSearchResult) {
+  const pageSectionValue = getPageSectionValue(row);
+  // Replace each segment of digits and non-digits with formatted strings
+  // Digits are padded with zeros to the left to ensure correct natural sorting
+  // Non-digits are left as is
+  return pageSectionValue.replace(/(\d+)|(\D+)/g, (_, $1, $2) =>
+    $1 ? Number($1).toString().padStart(10, '0') : $2,
+  );
+}
