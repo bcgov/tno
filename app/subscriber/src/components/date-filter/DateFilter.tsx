2c0a03c87a8863b154f775db1c799948d4ebc7cd
Jeremy Foster <jeremy.foster@fosol.ca>
2024-05-03 08:02:33 -0700

    Fix advanced search

--- app/subscriber/src/components/date-filter/DateFilter.tsx
+++ app/subscriber/src/components/date-filter/DateFilter.tsx
@@ -28,58 +28,36 @@ export const DateFilter: React.FC<IDateFilterProps> = ({
     setOpen(false);
   }, [filter.startDate, setOpen]);
 
-  // set initial DateFilter date
-  React.useEffect(() => {
-    if (!filter.startDate) {
-      const todaysDate = moment().toISOString();
-      handleDateChange({ startDate: todaysDate });
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [filter]);
-
-  /** update state variable when date changes, can be controlled via date picker or arrows */
-  React.useEffect(() => {
-    if (filter.startDate) {
-      handleDateChange({ startDate: filter.startDate });
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [filter.startDate]);
-
-  const handleDateChange = ({
-    startDate,
-    dateOffset,
-  }: {
-    startDate?: string;
-    dateOffset?: number;
-  }) => {
-    // must have at least an initial filter.startDate to go off, or an explicit startDate passed in
-    if (!filter.startDate && !startDate) return;
-
-    let newDate;
-    if (startDate) {
-      newDate = new Date(startDate);
-    } else if (filter.startDate && dateOffset) {
-      newDate = new Date(filter.startDate);
-      newDate.setDate(newDate.getDate() + dateOffset);
-    }
-
-    // only store filter there's no initial startDate yet, or if the new date differs from stored date
-    if (!filter.startDate || newDate?.toISOString() !== new Date(filter.startDate).toISOString()) {
+  const handleDateChange = React.useCallback(
+    ({ startDate, dateOffset }: { startDate?: string; dateOffset?: number }) => {
+      let newDate;
+      if (startDate && dateOffset === undefined) {
+        newDate = new Date(startDate);
+      } else if (startDate && dateOffset !== undefined) {
+        newDate = new Date(startDate);
+        newDate.setDate(newDate.getDate() + dateOffset);
+      }
       storeFilter({
         ...filter,
         startDate: moment(newDate).startOf('day').toISOString(),
         endDate: moment(newDate).endOf('day').toISOString(),
       });
-    }
-
-    if (onChangeDate) {
-      onChangeDate();
-    }
-  };
+      onChangeDate?.();
+    },
+    [filter, onChangeDate, storeFilter],
+  );
 
   return (
     <styled.DateFilter alignItems="center" justifyContent="center" className="date-navigator">
-      <FaCaretLeft className="caret" onClick={() => handleDateChange({ dateOffset: -1 })} />
+      <FaCaretLeft
+        className="caret"
+        onClick={() =>
+          handleDateChange({
+            startDate: filter.startDate ? filter.startDate : new Date().toISOString(),
+            dateOffset: -1,
+          })
+        }
+      />
       <div className="date">
         <FaCalendarDay className="calendar" onClick={() => setOpen(true)} />
         <ReactDatePicker
@@ -91,7 +69,15 @@ export const DateFilter: React.FC<IDateFilterProps> = ({
           selected={!!filter.startDate ? new Date(filter.startDate) : new Date()}
         />
       </div>
-      <FaCaretRight className="caret" onClick={() => handleDateChange({ dateOffset: 1 })} />
+      <FaCaretRight
+        className="caret"
+        onClick={() =>
+          handleDateChange({
+            startDate: filter.startDate ? filter.startDate : new Date().toISOString(),
+            dateOffset: 1,
+          })
+        }
+      />
     </styled.DateFilter>
   );
 };
