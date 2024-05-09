25fbcfe3f24a9c57d1918c268a5bf9e5c990cfc6
Jacob Wang <35620699+kkwangsir@users.noreply.github.com>
2024-05-08 23:25:02 -0700

    added toast to display someting

--- app/editor/src/features/admin/filters/utils/handleCopyKeyWords.ts
+++ app/editor/src/features/admin/filters/utils/handleCopyKeyWords.ts
@@ -1,3 +1,5 @@
+import { toast } from 'react-toastify';
+
 export const handleCopyKeyWords = (event: any, cell: any) => {
   navigator.clipboard.writeText(cell.original.settings!.search);
   // animate the clipboar icon to show something happened
@@ -5,4 +7,5 @@ export const handleCopyKeyWords = (event: any, cell: any) => {
   setTimeout(() => {
     event.target.classList.toggle('animate');
   }, 200);
+  toast.success('Keywords copied to clipboard');
 };
