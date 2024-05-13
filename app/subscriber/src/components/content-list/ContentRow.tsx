a5b057c226f6ca305e90af34be91969e7b70b110
jdtoombs <jdtoombs@gmail.com>
2024-05-13 15:36:31 -0700

    mini popout on same page

--- app/subscriber/src/components/content-list/ContentRow.tsx
+++ app/subscriber/src/components/content-list/ContentRow.tsx
@@ -81,7 +81,11 @@ export const ContentRow: React.FC<IContentRowProps> = ({
             className={`new-tab ${!item.section && 'no-section'}`}
             onClick={(e) => {
               e.stopPropagation();
-              window.open(`/view/${item.id}`, '_blank');
+              window.open(
+                `/view/${item.id}`,
+                '_blank',
+                'toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=600,width=600,height=465',
+              );
             }}
           />
         </Show>
