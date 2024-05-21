03b6b259aebf907302e78aa58557573cd2a6a97b
jdtoombs <jdtoombs@gmail.com>
2024-05-21 14:31:56 -0700

    1/2

--- app/subscriber/src/store/rootReducer.ts
+++ app/subscriber/src/store/rootReducer.ts
@@ -1,10 +1,7 @@
-import { loadingBarReducer } from 'react-redux-loading-bar';
-
 import { appSlice, contentSlice, lookupSlice, profileSlice, settingsSlice } from './slices';
 import { reportsSlice } from './slices/reports';
 
 export const reducer = {
-  loadingBar: loadingBarReducer,
   [appSlice.name]: appSlice.reducer,
   [profileSlice.name]: profileSlice.reducer,
   [contentSlice.name]: contentSlice.reducer,
