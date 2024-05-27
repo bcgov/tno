80b091092fbcc51ed368d577007cbf6ec45b836e
jdtoombs <jdtoombs@gmail.com>
2024-05-27 12:18:20 -0700

    lint

--- app/subscriber/src/components/content-list/utils/groupContent.ts
+++ app/subscriber/src/components/content-list/utils/groupContent.ts
@@ -39,38 +39,35 @@ export const groupContent = (groupBy: IGroupByState, content: IContentSearchResu
   const grouped = content
     .sort(sortFunc(firstSort))
     .sort(sortFunc(secondSort))
-    .reduce(
-      (acc, item) => {
-        let key = item.source?.name; // default to source
-        if (!item?.source?.name) {
-          key = item.otherSource;
-        }
-        switch (groupBy) {
-          case 'time':
-            const date = new Date(item.publishedOn);
-            const hours = date.getHours().toString().padStart(2, '0');
-            const minutes = date.getMinutes().toString().padStart(2, '0');
-            key = `${moment(date).format('DD-MMM-YYYY')} ${`(${hours}:${minutes})`}`;
-            break;
-          case 'source':
-            if (!item?.source?.name) {
-              key = item.otherSource;
-            } else {
-              const shortName =
-                item.source.shortName && item.source.shortName !== ''
-                  ? ` (${item.source.shortName})`
-                  : '';
-              key = `${item.source.name}${shortName}`;
-            }
-            break;
-        }
-        if (!acc[key]) {
-          acc[key] = [];
-        }
-        acc[key].push(item);
-        return acc;
-      },
-      {} as Record<string, IContentSearchResult[]>,
-    );
+    .reduce((acc, item) => {
+      let key = item.source?.name; // default to source
+      if (!item?.source?.name) {
+        key = item.otherSource;
+      }
+      switch (groupBy) {
+        case 'time':
+          const date = new Date(item.publishedOn);
+          const hours = date.getHours().toString().padStart(2, '0');
+          const minutes = date.getMinutes().toString().padStart(2, '0');
+          key = `${moment(date).format('DD-MMM-YYYY')} ${`(${hours}:${minutes})`}`;
+          break;
+        case 'source':
+          if (!item?.source?.name) {
+            key = item.otherSource;
+          } else {
+            const shortName =
+              item.source.shortName && item.source.shortName !== ''
+                ? ` (${item.source.shortName})`
+                : '';
+            key = `${item.source.name}${shortName}`;
+          }
+          break;
+      }
+      if (!acc[key]) {
+        acc[key] = [];
+      }
+      acc[key].push(item);
+      return acc;
+    }, {} as Record<string, IContentSearchResult[]>);
   return grouped;
 };
