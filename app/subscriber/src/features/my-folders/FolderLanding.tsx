3ef638021fc2b56b545f43e261f282ca9de67452
Felipe Barreta <fbarreta@gmail.com>
2024-05-28 16:55:40 -0700

    Tno 2647 default folder route (#1870)
    * TNO-2647 Default action route to view
    
    * TNO-2647 Use navigate to redirect to the view

--- app/subscriber/src/features/my-folders/FolderLanding.tsx
+++ app/subscriber/src/features/my-folders/FolderLanding.tsx
@@ -1,7 +1,7 @@
 import { PageSection } from 'components/section';
 import { ManageFolder } from 'features/manage-folder';
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { Col, Show } from 'tno-core';
 
 import { ConfigureFolder } from './ConfigureFolder';
@@ -10,7 +10,14 @@ import { MyFolders } from './MyFolders';
 import * as styled from './styled';
 
 export const FolderLanding: React.FC<{}> = () => {
-  const { action } = useParams();
+  const { action, id } = useParams();
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    if (action === undefined && id) {
+      navigate(`/folders/view/${id}`);
+    }
+  }, [action, id, navigate]);
 
   return (
     <styled.FolderLanding split={!!action}>
