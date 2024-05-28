db44520fc91b08993c201a14fb2f36b52ae03f34
jdtoombs <jdtoombs@gmail.com>
2024-05-27 17:29:15 -0700

    update editor pkgs

--- app/editor/src/store/store.ts
+++ app/editor/src/store/store.ts
@@ -1,6 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import { loadingBarMiddleware } from 'react-redux-loading-bar';
 import logger from 'redux-logger';
 
 import { reducer } from './rootReducer';
@@ -8,8 +7,7 @@ import { reducer } from './rootReducer';
 export type ActionDelegate<T> = T | ((values: T) => T);
 export const store = configureStore({
   reducer: reducer,
-  middleware: (getDefaultMiddleware: () => any[]) =>
-    getDefaultMiddleware().concat(logger).concat(loadingBarMiddleware()),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
   devTools: process.env.NODE_ENV !== 'production',
 });
 export type AppDispatch = typeof store.dispatch;
