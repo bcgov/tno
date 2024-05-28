6c98085152a7a11a53a7e31b484c870473afa57c
jdtoombs <jdtoombs@gmail.com>
2024-05-28 14:03:29 -0700

    responsive

--- app/subscriber/src/features/my-folders/styled/FolderLanding.tsx
+++ app/subscriber/src/features/my-folders/styled/FolderLanding.tsx
@@ -4,10 +4,21 @@ import { Row } from 'tno-core';
 export const FolderLanding = styled(Row)<{ split?: boolean }>`
   width: 100%;
   .left-side {
-    width: ${({ split }) => (split ? '30%' : '100%')};
+    @media (max-width: 1100px) {
+      width: 100%;
+    }
+    @media (min-width: 1100px) {
+      width: ${({ split }) => (split ? '30%' : '100%')};
+    }
+    transition: width 0.5s ease-in-out;
   }
   .right-side {
-    width: ${({ split }) => (split ? '70%' : '0%')};
+    @media (max-width: 1100px) {
+      width: 100%;
+    }
+    @media (min-width: 1100px) {
+      width: ${({ split }) => (split ? '70%' : '100%')};
+    }
     transition: width 0.5s ease-in-out;
   }
 `;
