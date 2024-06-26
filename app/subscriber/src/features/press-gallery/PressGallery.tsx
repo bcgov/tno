caab8a8377f98ae94f1395fc23c1cd76fee98a9e
Jonny Toombs <jdtoombs@gmail.com>
2024-06-26 11:27:41 -0700

    fix attrb display (#1985)

--- app/subscriber/src/features/press-gallery/PressGallery.tsx
+++ app/subscriber/src/features/press-gallery/PressGallery.tsx
@@ -229,6 +229,9 @@ export const PressGallery: React.FC = () => {
       <ContentList
         onContentSelected={handleContentSelected}
         content={content}
+        showDate
+        showTime
+        showSeries
         selected={selected}
       />
     </styled.PressGallery>
