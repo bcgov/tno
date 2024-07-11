7db2f496fcbe5f968cf1cbd0170122604a3c6368
Jonny Toombs <jdtoombs@gmail.com>
2024-07-11 12:31:13 -0700

    TNO-2785: Manage product sub requests  (#2009)
    * request managment for product subs
    
    * few fixes

--- app/editor/src/features/admin/products/styled/index.ts
+++ app/editor/src/features/admin/products/styled/index.ts
@@ -2,4 +2,5 @@ export * from './ProductDetailsForm';
 export * from './ProductForm';
 export * from './ProductList';
 export * from './ProductListFilter';
+export * from './ProductSubRequests';
 export * from './ProductSubscribersForm';
