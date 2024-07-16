6e84aa0ffb537a913ecbb86c16e5e225bad6b017
Jeremy Foster <jeremy.foster@fosol.ca>
2024-07-16 14:08:05 -0700

    TNO-2885 Fix advanced search

--- libs/npm/core/src/utils/elastic/generateTextQuery.ts
+++ libs/npm/core/src/utils/elastic/generateTextQuery.ts
@@ -7,28 +7,30 @@ export const generateTextQuery = (
 ) => {
   const containsWildCards = settings.search?.includes('*') || settings.search?.includes('?');
   const generateQueryFunction = containsWildCards ? generateQueryString : generateSimpleQueryString;
-  if (!settings.search && !settings.headline && !settings.summary && !settings.body)
+  if (
+    !settings.search &&
+    !settings.headline &&
+    !settings.summary &&
+    !settings.body &&
+    !settings.inProgram
+  )
     return undefined;
-  if (!!settings.inHeadline && !!settings.inByline && !!settings.inStory) {
-    // give an arbitrary weight to the headline, so if it's found there
-    // it gets a slightly higher score, as opposed to other fields
-    return generateQueryFunction(
-      ['headline^5', 'byline', 'summary', 'body', 'series.name'],
-      settings.search,
-      settings.defaultSearchOperator,
-    );
-  }
 
   let fields: string[] = [];
   if (settings.inByline || settings.inStory || settings.inHeadline || settings.inProgram) {
-    if (!!settings.inByline) fields = [...fields, 'content.contributor.name'];
+    if (!!settings.inByline) fields = [...fields, 'byline', 'content.contributor.name'];
     if (!!settings.inStory) fields = [...fields, 'summary', 'body'];
-    if (!!settings.inHeadline) fields = [...fields, 'headline'];
+    if (!!settings.inHeadline) fields = [...fields, 'headline^5'];
     if (!!settings.inProgram) fields = [...fields, 'series.name'];
-    return fields.length > 0 ? generateQueryFunction(fields, settings.search) : undefined;
+    return fields.length > 0
+      ? generateQueryFunction(fields, settings.search, settings.defaultSearchOperator)
+      : undefined;
   }
 
-  if (settings.headline) return generateQueryFunction(['headline'], settings.headline);
-  if (settings.summary) return generateQueryFunction(['summary'], settings.summary);
-  if (settings.body) return generateQueryFunction(['body'], settings.body);
+  if (settings.headline)
+    return generateQueryFunction(['headline'], settings.headline, settings.defaultSearchOperator);
+  if (settings.summary)
+    return generateQueryFunction(['summary'], settings.summary, settings.defaultSearchOperator);
+  if (settings.body)
+    return generateQueryFunction(['body'], settings.body, settings.defaultSearchOperator);
 };
