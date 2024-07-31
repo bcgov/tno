7bc96f573cb55f1169f9e1347323707803fc669c
Jonny Toombs <jdtoombs@gmail.com>
2024-07-31 12:33:36 -0700

    tno-2911 (#2114)

--- app/subscriber/src/features/press-gallery/utils/generateDates.ts
+++ app/subscriber/src/features/press-gallery/utils/generateDates.ts
@@ -5,7 +5,7 @@ export const generateDates = () => {
   const dates: any = Array.from({ length: 7 }, (_, i) => {
     const date = moment().subtract(i, 'days');
     return {
-      label: date.format('YYYY-MM-DD'),
+      label: date.format('MMMM DD - YYYY'),
       value: date.format(),
     };
   });
