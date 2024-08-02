0704c8c1d6a98bba2dda73f4d5709c2c8c950906
nehalaggarwal-bcgov <nehal.aggarwal@gov.bc.ca>
2024-08-02 16:21:20 -0700

    TNO-2777 code to stick buttons to bottom (#2128)

--- app/subscriber/src/features/my-reports/edit/styled/ReportEditForm.tsx
+++ app/subscriber/src/features/my-reports/edit/styled/ReportEditForm.tsx
@@ -4,6 +4,7 @@ export const ReportEditForm = styled.div`
   display: flex;
   flex-direction: column;
   overflow-x: clip;
+  min-height: 89vh;
 
   .preview-report {
     border: solid 2px ${(props) => props.theme.css.linePrimaryColor};
