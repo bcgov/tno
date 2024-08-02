2c84f805350d84266955796eafd462778ef9cdfc
nehalaggarwal-bcgov <nehal.aggarwal@gov.bc.ca>
2024-08-02 12:17:21 -0700

    TNO-2777 added css code to stick buttons to report tab bottom

--- app/subscriber/src/features/my-reports/edit/styled/ReportEditForm.tsx
+++ app/subscriber/src/features/my-reports/edit/styled/ReportEditForm.tsx
@@ -4,7 +4,7 @@ export const ReportEditForm = styled.div`
   display: flex;
   flex-direction: column;
   overflow-x: clip;
-  height: 100vh;
+  min-height: 89dvh;
 
   .preview-report {
     border: solid 2px ${(props) => props.theme.css.linePrimaryColor};
