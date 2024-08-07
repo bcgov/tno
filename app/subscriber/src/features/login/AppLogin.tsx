3971cacbc18f2d08c57dd07656e34eaf2adfd046
Jonny Toombs <jdtoombs@gmail.com>
2024-08-07 13:10:25 -0700

    Revamp log-in page (#2140)
    * revamp login
    
    * few fixes

---
+++ app/subscriber/src/features/login/AppLogin.tsx
@@ -0,0 +1,66 @@
+import { LightweightModal } from 'components/lightweight-modal/LightweightModal';
+import React from 'react';
+
+import { IDPOptions } from './IDPOptions';
+import * as styled from './styled';
+import { SystemMessage } from './SystemMessage';
+
+export interface IAppLoginProps {
+  login: Function;
+}
+
+export const AppLogin: React.FC<IAppLoginProps> = ({ login }) => {
+  const [showModal, setShowModal] = React.useState(false);
+  return (
+    <styled.AppLogin>
+      <img alt="MMI Logo" className="app-logo" src="/assets/MMinsights_logo_dark_text.svg" />
+      <div className="login-box">
+        <div className="top-bar">
+          Media Monitoring is a paid service offered through the BC Government that allows
+          subscribers to see British Columbia’s news at a glance.
+        </div>
+        <IDPOptions login={login}>
+          <div className="learn-more" onClick={() => setShowModal(true)}>
+            Learn more about obtaining a subscription...
+          </div>
+        </IDPOptions>
+      </div>
+      <SystemMessage />
+      <LightweightModal open={showModal} close={() => setShowModal(false)}>
+        <div className="modal-content">
+          <div>
+            <p>
+              <b>Thank you for your interest in Media Monitoring Insights.</b>
+            </p>
+            <p>
+              <b>Key features:</b>
+            </p>
+            <ul>
+              <li>Aggregation of newspapers, radio shows, and online articles</li>
+              <li>Transcription services</li>
+              <li>BC’s top stories as they break</li>
+              <li>Articles related to major stories</li>
+            </ul>
+            <p>
+              Access to this service is limited to Government of British Columbia staff as per our
+              agreements with our media suppliers.
+            </p>
+            <br />
+            <p>
+              <b>
+                If you are eligible for access, email{' '}
+                <a href="mailto:Scott.Ryckman@gov.bc.ca">Scott.Ryckman@gov.bc.ca</a> to get set up.
+              </b>
+            </p>
+            <br />
+            <p>
+              Note: <br />
+              There is a cost recovery charge in excess of $4,500 annually, please ensure that you
+              have authorization for this purchase prior to emailing.
+            </p>
+          </div>
+        </div>
+      </LightweightModal>
+    </styled.AppLogin>
+  );
+};
--- app/subscriber/src/features/login/styled/SystemMessage.tsx
+++ app/subscriber/src/features/login/styled/SystemMessage.tsx
@@ -1,24 +1,17 @@
 import styled from 'styled-components';
 
 export const SystemMessage = styled.div`
-  .system-message-containing-box {
-    padding: 1%;
-  }
-
   .system-message-box {
-    display: inline-block;
-    position: relative;
-    padding: 2%;
-    height: 410px;
-    float: left;
-    background-color: ${(props) => props.theme.css.stickyNoteColor};
-    @media (max-width: 768px) {
-      margin-bottom: 0.5em;
-      width: 92%;
-    }
-    @media (min-width: 1450px) {
-      max-width: 50em;
-      margin-left: 1em;
-    }
+    border: 1px solid ${(props) => props.theme.css.btnBkPrimary};
+    max-width: 73em;
+    margin-right: auto;
+    margin-left: auto;
+    border-radius: 0.5em;
+    padding: 0.5em;
+  }
+  margin-top: 1.5em;
+  .bell-icon {
+    float: right;
+    color: ${(props) => props.theme.css.btnBkPrimary};
   }
 `;
