3fc32c66372f53516946cbaec06ac6ec68cc9eac
Jonny Toombs <jdtoombs@gmail.com>
2024-08-14 20:17:10 -0700

    TNO-2905: Autoplay inline video (#2175)
    * autoplay inline video
    
    * fix bugs

--- app/subscriber/src/components/content-list/ContentRow.tsx
+++ app/subscriber/src/components/content-list/ContentRow.tsx
@@ -50,6 +50,8 @@ export const ContentRow: React.FC<IContentRowProps> = ({
     activeStream,
   } = React.useContext(ContentListContext);
 
+  const videoRef = React.useRef<HTMLVideoElement>(null);
+
   const body = React.useMemo(() => {
     const truncated = truncateTeaser(item.body || item.summary, 250);
     return formatSearch(truncated, filter);
@@ -60,6 +62,24 @@ export const ContentRow: React.FC<IContentRowProps> = ({
 
   const bodyTermHighlighted = highlightTerms(body as string, highlighTerms ?? []);
   const headerTermHighlighted = highlightTerms(headline as string, highlighTerms ?? []);
+
+  /** This is a workaround for the autoplay attribute on <video>. Most chromium browser do not allow autoplay with the muted attribute included; however, we can avoid this issue by programatically calling play. The logic below will play the video
+   * once the user clicks the inline play button. */
+  React.useEffect(() => {
+    const videoElement = videoRef.current;
+    if (!!videoElement && activeStream?.source) {
+      const handleCanPlay = () => {
+        videoElement.play().catch((e) => console.error('Failed to play video', e));
+      };
+
+      // need to have an event listener to trigger the play, otherwise it will error out on the play call (calls too early)
+      videoElement.addEventListener('canplay', handleCanPlay);
+
+      return () => {
+        videoElement.removeEventListener('canplay', handleCanPlay);
+      };
+    }
+  }, [activeStream]);
 
   return (
     <styled.ContentRow simpleView={simpleView} {...rest}>
@@ -202,7 +222,7 @@ export const ContentRow: React.FC<IContentRowProps> = ({
               <audio controls src={activeStream?.source} />
             )}
             {activeFileReference?.contentType.includes('video') && (
-              <video controls src={activeStream?.source} />
+              <video controls src={activeStream?.source} ref={videoRef} />
             )}
             <div className="copyright-text">
               <FaCopyright />
