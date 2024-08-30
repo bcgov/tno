4ae173d27589abe3f0055273199d1c00534ba3c4
Dao Yu Lai <daoyu.lai@gov.bc.ca>
2024-08-30 15:27:31 -0700

    TNO-2978 Replace Flex Box table
    - replaced Ingest Services Flex Box table with Grid
    - removed ingest list columns definitions file

--- app/editor/src/features/admin/ingests/IngestList.tsx
+++ app/editor/src/features/admin/ingests/IngestList.tsx
@@ -3,9 +3,18 @@ import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { useIngests } from 'store/hooks/admin';
 import { useAdminStore } from 'store/slices';
-import { Col, FlexboxTable, IconButton, Row } from 'tno-core';
+import {
+  CellCheckbox,
+  CellDate,
+  CellEllipsis,
+  Col,
+  Grid,
+  IconButton,
+  IIngestModel,
+  Row,
+  SortDirection,
+} from 'tno-core';
 
-import { columns } from './constants';
 import { IngestFilter } from './IngestFilter';
 import * as styled from './styled';
 import { getStatus } from './utils';
@@ -16,21 +25,98 @@ const IngestList: React.FC<IIngestListProps> = (props) => {
   const navigate = useNavigate();
   const [{ ingests }, { findAllIngests, setIngestEnabledStatus }] = useIngests();
   const [{ ingestFilter }] = useAdminStore();
-
-  const items = ingests.filter(
-    (i) =>
-      i.name.toLocaleLowerCase().includes(ingestFilter) ||
-      i.source?.code.toLocaleLowerCase().includes(ingestFilter) ||
-      i.description.toLocaleLowerCase().includes(ingestFilter) ||
-      i.ingestType?.name.toLocaleLowerCase().includes(ingestFilter) ||
-      getStatus(i).toLocaleLowerCase().includes(ingestFilter),
-  );
+  const [isReady, setIsReady] = React.useState(false);
+  const [items, setItems] = React.useState<IIngestModel[]>([]);
 
   React.useEffect(() => {
-    findAllIngests().catch(() => {});
-    // Only init list.
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    if (!ingests.length && !isReady) {
+      setIsReady(true);
+      findAllIngests()
+        .then((data) => {
+          setItems(
+            data.filter(
+              (i) =>
+                i.name.toLocaleLowerCase().includes(ingestFilter) ||
+                i.source?.code.toLocaleLowerCase().includes(ingestFilter) ||
+                i.description.toLocaleLowerCase().includes(ingestFilter) ||
+                i.ingestType?.name.toLocaleLowerCase().includes(ingestFilter) ||
+                getStatus(i).toLocaleLowerCase().includes(ingestFilter),
+            ),
+          );
+        })
+        .catch(() => {});
+    } else {
+      setItems(
+        ingests.filter(
+          (i) =>
+            i.name.toLocaleLowerCase().includes(ingestFilter) ||
+            i.source?.code.toLocaleLowerCase().includes(ingestFilter) ||
+            i.description.toLocaleLowerCase().includes(ingestFilter) ||
+            i.ingestType?.name.toLocaleLowerCase().includes(ingestFilter) ||
+            getStatus(i).toLocaleLowerCase().includes(ingestFilter),
+        ),
+      );
+    }
+  }, [findAllIngests, isReady, ingests, ingestFilter]);
+
+  const handleOnSorting = (column: any, direction: any) => {
+    if (direction && column.name) {
+      setItems((items) => {
+        switch (column.name) {
+          case 'source.code':
+            return [...items].sort((a: any, b: any) => {
+              const aValue = a['source']['code'];
+              const bValue = b['source']['code'];
+              if (aValue > bValue) return direction === SortDirection.Ascending ? 1 : -1;
+              if (aValue < bValue) return direction === SortDirection.Ascending ? -1 : 1;
+              return 0;
+            });
+          case 'ingestType.name':
+            return [...items].sort((a: any, b: any) => {
+              const aValue = a['ingestType']['name'];
+              const bValue = b['ingestType']['name'];
+              if (aValue > bValue) return direction === SortDirection.Ascending ? 1 : -1;
+              if (aValue < bValue) return direction === SortDirection.Ascending ? -1 : 1;
+              return 0;
+            });
+          case 'status':
+            return [...items].sort((a: any, b: any) => {
+              const aValue = getStatus(a);
+              const bValue = getStatus(b);
+              if (aValue > bValue) return direction === SortDirection.Ascending ? 1 : -1;
+              if (aValue < bValue) return direction === SortDirection.Ascending ? -1 : 1;
+              return 0;
+            });
+          case 'isEnabled':
+            return [...items].sort((a: any, b: any) => {
+              const aValue = a[column.name!] as boolean;
+              const bValue = b[column.name!] as boolean;
+              if (aValue === bValue) return 0;
+              if (aValue) {
+                return direction === SortDirection.Ascending ? -1 : 1;
+              } else {
+                return direction === SortDirection.Ascending ? 1 : -1;
+              }
+            });
+          default:
+            return [...items].sort((a: any, b: any) => {
+              const aValue = a[column.name!] ?? '';
+              const bValue = b[column.name!] ?? '';
+              if (aValue > bValue) return direction === SortDirection.Ascending ? 1 : -1;
+              if (aValue < bValue) return direction === SortDirection.Ascending ? -1 : 1;
+              return 0;
+            });
+        }
+      });
+    } else {
+      setItems(ingests);
+    }
+  };
+
+  const handleEnabledClicked = (event: any, row: any) => {
+    setIngestEnabledStatus(row.id, !row.isEnabled).catch(() => {});
+    toast.success(`Ingest [${row.name}] is now ` + (row.isEnabled ? `disabled` : `enabled`));
+  };
 
   return (
     <styled.IngestList>
@@ -46,22 +132,79 @@ const IngestList: React.FC<IIngestListProps> = (props) => {
         />
       </Row>
       <IngestFilter />
-      <FlexboxTable
-        rowId="id"
-        data={items}
-        columns={columns}
-        showSort={true}
-        onCellClick={async (cell, row) => {
-          if (cell.index !== 6) navigate(`${row.original.id}`);
-          else {
-            await setIngestEnabledStatus(row.original.id, !row.original.isEnabled).catch(() => {});
-            toast.success(
-              `Ingest [${row.original.name}] is now ` +
-                (row.original.isEnabled ? `disabled` : `enabled`),
-            );
-          }
+      <Grid
+        items={items}
+        showPaging={false}
+        onSortChange={(column, direction) => handleOnSorting(column, direction)}
+        renderHeader={() => [
+          { name: 'name', label: 'Name', size: '23%', sortable: true },
+          { name: 'source.code', label: 'Source', size: '10%', sortable: true },
+          { name: 'description', label: 'Description', size: '25%', sortable: true },
+          { name: 'ingestType.name', label: 'Type', size: '10%', sortable: true },
+          { name: 'status', label: 'Status', size: '8%', sortable: true },
+          { name: 'lastRanOn', label: 'Last Run', size: '18%', sortable: true },
+          { name: 'isEnabled', label: 'Enabled', sortable: true },
+        ]}
+        renderColumns={(row: IIngestModel, rowIndex) => {
+          return [
+            {
+              column: (
+                <div key="" className="clickable" onClick={() => navigate(`${row.id}`)}>
+                  <CellEllipsis key="">{row.name}</CellEllipsis>
+                </div>
+              ),
+            },
+            {
+              column: (
+                <div key="" className="clickable" onClick={() => navigate(`${row.id}`)}>
+                  <CellEllipsis key="">{row.source?.code}</CellEllipsis>
+                </div>
+              ),
+            },
+            {
+              column: (
+                <div key="" className="clickable" onClick={() => navigate(`${row.id}`)}>
+                  <CellEllipsis key="">{row.description}</CellEllipsis>
+                </div>
+              ),
+            },
+            {
+              column: (
+                <div key="" className="clickable" onClick={() => navigate(`${row.id}`)}>
+                  <CellEllipsis key="">{row.ingestType?.name}</CellEllipsis>
+                </div>
+              ),
+            },
+            {
+              column: (
+                <div key="" className="clickable" onClick={() => navigate(`${row.id}`)}>
+                  <CellEllipsis key="">{getStatus(row)}</CellEllipsis>
+                </div>
+              ),
+            },
+            {
+              column: (
+                <div key="" className="clickable" onClick={() => navigate(`${row.id}`)}>
+                  <CellDate value={row.lastRanOn} />
+                </div>
+              ),
+            },
+            {
+              column: (
+                <div
+                  key=""
+                  className="clickable"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    handleEnabledClicked(e, row);
+                  }}
+                >
+                  <CellCheckbox key="" checked={row.isEnabled} />
+                </div>
+              ),
+            },
+          ];
         }}
-        pagingEnabled={false}
       />
     </styled.IngestList>
   );
