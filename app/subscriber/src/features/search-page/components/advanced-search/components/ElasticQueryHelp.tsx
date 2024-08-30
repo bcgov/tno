9a73ec4f402332c0de24375bf292a7590ea7bd8f
Jonny Toombs <jdtoombs@gmail.com>
2024-08-29 18:32:34 -0700

    draggable elastic info (#2237)

--- app/subscriber/src/features/search-page/components/advanced-search/components/ElasticQueryHelp.tsx
+++ app/subscriber/src/features/search-page/components/advanced-search/components/ElasticQueryHelp.tsx
@@ -1,5 +1,7 @@
-import { TooltipMenu } from 'components/tooltip-menu';
+import React from 'react';
+import Draggable from 'react-draggable';
 import { FaInfoCircle } from 'react-icons/fa';
+import { FaX } from 'react-icons/fa6';
 import { Col, Row, Show } from 'tno-core';
 
 import * as styled from './styled';
@@ -12,76 +14,90 @@ export interface IElasticQueryHelpProps {
 export const ElasticQueryHelp: React.FC<IElasticQueryHelpProps> = ({
   queryType = 'simple-query-string',
 }) => {
+  const [show, setShow] = React.useState(false);
+  const nodeRef = React.useRef<HTMLDivElement>(null);
+
+  const handleInfoClick = () => {
+    setShow(!show);
+  };
+
   return (
     <styled.ElasticInfo className="elastic-info">
-      <FaInfoCircle data-tooltip-id="elastic-info" className="info-icon" />
-      <TooltipMenu openOnClick id="elastic-info">
-        <Show visible={queryType === 'simple-query-string'}>
-          <Col>
-            The keywords query supports the following operators:
-            <ul>
-              <Row className="row">
-                <code>+</code> signifies AND operation
+      <FaInfoCircle onClick={handleInfoClick} className="info-icon" />
+      <Show visible={show}>
+        <Draggable nodeRef={nodeRef}>
+          <div ref={nodeRef}>
+            <Show visible={queryType === 'simple-query-string'}>
+              <Row>
+                <FaX onClick={() => setShow(false)} className="close-icon" />
               </Row>
-              <Row className="row">
-                <code>|</code> signifies OR operation
-              </Row>
-              <Row className="row">
-                <code>-</code> negates a single token
-              </Row>
-              <Row className="row">
-                <code>"</code> wraps a number of tokens to signify a phrase for searching
-              </Row>
-              <Row className="row">
-                <code>*</code> at the end of a term signifies a prefix query
-              </Row>
-              <Row className="row">
-                <code>(</code> and <code>)</code> signify precedence
-              </Row>
-              <Row className="row">
-                <code>~N</code> after a word signifies edit distance (fuzziness)
-              </Row>
-              <Row className="row">
-                <code>~N</code> after a phrase signifies slop amount
-              </Row>
-            </ul>
-          </Col>
-        </Show>
-        <Show visible={queryType === 'query-string'}>
-          <Col>
-            The keywords query supports the following operators:
-            <ul>
-              <Row className="row">
-                <code>AND</code> <code>&&</code> <code>+</code> signifies AND operation
-              </Row>
-              <Row className="row">
-                <code>OR</code> <code>||</code> signifies OR operation
-              </Row>
-              <Row className="row">
-                <code>NOT</code> <code>!</code> <code>-</code> negates a single token
-              </Row>
-              <Row className="row">
-                <code>"</code> wraps a number of tokens to signify a phrase for searching
-              </Row>
-              <Row className="row">
-                <code>*</code> wildcard characters
-              </Row>
-              <Row className="row">
-                <code>?</code> wildcard character
-              </Row>
-              <Row className="row">
-                <code>(</code> and <code>)</code> signify precedence
-              </Row>
-              <Row className="row">
-                <code>~N</code> after a word signifies edit distance (fuzziness)
-              </Row>
-              <Row className="row">
-                <code>~N</code> after a phrase signifies slop amount
-              </Row>
-            </ul>
-          </Col>
-        </Show>
-      </TooltipMenu>
+              <Col>
+                The keywords query supports the following operators:
+                <ul>
+                  <Row className="row">
+                    <code>+</code> signifies AND operation
+                  </Row>
+                  <Row className="row">
+                    <code>|</code> signifies OR operation
+                  </Row>
+                  <Row className="row">
+                    <code>-</code> negates a single token
+                  </Row>
+                  <Row className="row">
+                    <code>"</code> wraps a number of tokens to signify a phrase for searching
+                  </Row>
+                  <Row className="row">
+                    <code>*</code> at the end of a term signifies a prefix query
+                  </Row>
+                  <Row className="row">
+                    <code>(</code> and <code>)</code> signify precedence
+                  </Row>
+                  <Row className="row">
+                    <code>~N</code> after a word signifies edit distance (fuzziness)
+                  </Row>
+                  <Row className="row">
+                    <code>~N</code> after a phrase signifies slop amount
+                  </Row>
+                </ul>
+              </Col>
+            </Show>
+            <Show visible={queryType === 'query-string'}>
+              <Col>
+                The keywords query supports the following operators:
+                <ul>
+                  <Row className="row">
+                    <code>AND</code> <code>&&</code> <code>+</code> signifies AND operation
+                  </Row>
+                  <Row className="row">
+                    <code>OR</code> <code>||</code> signifies OR operation
+                  </Row>
+                  <Row className="row">
+                    <code>NOT</code> <code>!</code> <code>-</code> negates a single token
+                  </Row>
+                  <Row className="row">
+                    <code>"</code> wraps a number of tokens to signify a phrase for searching
+                  </Row>
+                  <Row className="row">
+                    <code>*</code> wildcard characters
+                  </Row>
+                  <Row className="row">
+                    <code>?</code> wildcard character
+                  </Row>
+                  <Row className="row">
+                    <code>(</code> and <code>)</code> signify precedence
+                  </Row>
+                  <Row className="row">
+                    <code>~N</code> after a word signifies edit distance (fuzziness)
+                  </Row>
+                  <Row className="row">
+                    <code>~N</code> after a phrase signifies slop amount
+                  </Row>
+                </ul>
+              </Col>
+            </Show>
+          </div>
+        </Draggable>
+      </Show>
     </styled.ElasticInfo>
   );
 };
