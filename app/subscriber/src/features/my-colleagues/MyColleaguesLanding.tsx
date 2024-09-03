98900ab33396f2d6821860ba6579901782cf9856
nehalaggarwal-bcgov <nehal.aggarwal@gov.bc.ca>
2024-09-03 12:21:21 -0700

    TNO-2930 changes to my colleagues page, unused pages need to be removed.

--- app/subscriber/src/features/my-colleagues/MyColleaguesLanding.tsx
+++ app/subscriber/src/features/my-colleagues/MyColleaguesLanding.tsx
@@ -2,11 +2,11 @@ import { Action } from 'components/action';
 import { Button } from 'components/button';
 import { Modal } from 'components/modal';
 import React from 'react';
-import { FaTrash, FaUserPlus } from 'react-icons/fa';
+import { FaTrash, FaUserPlus, FaUsers } from 'react-icons/fa';
 import { toast } from 'react-toastify';
 import { useColleagues } from 'store/hooks';
 import { useProfileStore } from 'store/slices';
-import { Col, Grid, IUserColleagueModel, Row, Text, useModal, validateEmail } from 'tno-core';
+import { Col, Grid, IUserColleagueModel, Text, useModal, validateEmail } from 'tno-core';
 
 import * as styled from './styled';
 
@@ -15,7 +15,7 @@ export const MyColleaguesLanding = () => {
   const [{ getColleagues, deleteColleague, addColleague }] = useColleagues();
   const { toggle, isShowing } = useModal();
   const [colleague, setColleague] = React.useState<IUserColleagueModel>();
-  const [emailForAdd, setEmailForAdd] = React.useState('');
+  const [colleagueEmail, setEmailForAdd] = React.useState('');
 
   React.useEffect(() => {
     if (!init.myColleagues) {
@@ -35,14 +35,15 @@ export const MyColleaguesLanding = () => {
     [deleteColleague],
   );
 
-  const [colleagueEmail] = React.useState('');
-
   const handleSubmit = React.useCallback(
     async (values: string) => {
       try {
         await addColleague(values);
+        setEmailForAdd('');
+        toast.success(`Successfully added '${values}' as colleague.`);
       } catch {
-        toast.warning(`No users found for the specified email "${values}".`);
+        //toast.warning(`No users found for the specified email "${values}".`);
+        //error message handled by addColleague()
       }
     },
     [addColleague],
@@ -50,81 +51,81 @@ export const MyColleaguesLanding = () => {
 
   return (
     <styled.MyColleagues>
-      <div className="header-row">
-        <FaUserPlus className="icon" />
-        <span className="header-text">Add a Colleague</span>
-      </div>
-      <p className="description">
-        Add MMI users that you would like to be able to quickly share stories with. From any of the
-        story lists, or from the story itself, you can then choose to "share" and find your
-        colleagues in this list.
-      </p>
-      <Row gap="1rem">
-        <Col flex="1">
-          <div className="colleague-block">
-            <div>
-              <FaUserPlus size={20} />
-            </div>
-            <Col>
-              <div className="subscriber-title">Add a Colleague</div>
-              <div className="subscriber-describe">
-                Subscribers will receive this report by email each time it is sent out. To be added,
-                a person must have an active MMI account (direct or indirect).
-              </div>{' '}
-              <Text
-                name="email"
-                label="FIND BY EMAIL"
-                value={colleagueEmail}
-                onChange={(e) => setEmailForAdd(e.target.value)}
-                width="300px"
+      <div className="colleague-block">
+        <Col>
+          <div className="colleague-title">
+            <FaUserPlus className="icon" />
+            <span>Add a Colleague</span>
+          </div>
+          <div className="colleague-body">
+            <div className="colleague-describe">
+              Add MMI users that you would like to be able to quickly share stories with. From any
+              of the story lists, or from the story itself, you can then choose to "share" and find
+              your colleagues in this list.
+            </div>{' '}
+            <Text
+              name="email"
+              label="FIND BY EMAIL:"
+              value={colleagueEmail}
+              onChange={(e) => setEmailForAdd(e.target.value)}
+              width="300px"
+            >
+              <Button
+                className="request-button"
+                variant="secondary"
+                disabled={!validateEmail(colleagueEmail)}
+                onClick={() => handleSubmit(colleagueEmail)}
+                style={{ backgroundColor: 'transparent' }}
               >
-                <Button
-                  className="request-button"
-                  variant="secondary"
-                  disabled={!validateEmail(emailForAdd)}
-                  onClick={() => handleSubmit(emailForAdd)}
-                  style={{ backgroundColor: 'transparent' }}
-                >
-                  <FaUserPlus />
-                  Add
-                </Button>
-              </Text>
-            </Col>
+                <FaUserPlus />
+                Add
+              </Button>
+            </Text>
           </div>
         </Col>
-      </Row>
+      </div>
       <div className="colleague-block">
-        <FaUserPlus size={20} />
-        <Col className="subscribers">
-          <Row justifyContent="space-between">
-            <div className="subscriber-title">My Colleagues</div>
-          </Row>
-          <Grid
-            items={myColleagues}
-            renderHeader={() => {
-              return [
-                <div key="username">Username</div>,
-                <div key="firstName">First Name</div>,
-                <div key="lastName">Last Name</div>,
-                <div key="email">Email</div>,
-              ];
-            }}
-            renderColumns={(row) => {
-              return [
-                <div key="username">{row.colleague?.username}</div>,
-                <div key="firstName">{row.colleague?.firstName}</div>,
-                <div key="lastName">{row.colleague?.lastName}</div>,
-                <div key="email">
-                  {row.colleague?.preferredEmail.length
-                    ? row.colleague?.preferredEmail
-                    : row.colleague?.email}
-                </div>,
-                <div key="actions">
-                  <Action icon={<FaTrash />} onClick={() => deleteColleague(row)} />
-                </div>,
-              ];
-            }}
-          />
+        <Col>
+          <div className="colleague-title">
+            <FaUsers className="icon" />
+            <span>My Colleagues</span>
+          </div>
+          <div className="colleague-body">
+            <Grid
+              className="colleague-table"
+              items={myColleagues}
+              renderHeader={() => {
+                return [
+                  <div key="username">Username</div>,
+                  <div key="firstName">First Name</div>,
+                  <div key="lastName">Last Name</div>,
+                  <div key="email">Email</div>,
+                  <div key="trash"></div>,
+                ];
+              }}
+              renderColumns={(row) => {
+                return [
+                  <div key="username">{row.colleague?.username}</div>,
+                  <div key="firstName">{row.colleague?.firstName}</div>,
+                  <div key="lastName">{row.colleague?.lastName}</div>,
+                  <div key="email">
+                    {row.colleague?.preferredEmail.length
+                      ? row.colleague?.preferredEmail
+                      : row.colleague?.email}
+                  </div>,
+                  <div key="actions">
+                    <Action
+                      icon={<FaTrash />}
+                      onClick={() => {
+                        setColleague(row);
+                        toggle();
+                      }}
+                    />
+                  </div>,
+                ];
+              }}
+            />
+          </div>
         </Col>
       </div>
       <Modal
