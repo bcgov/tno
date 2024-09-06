245c94d3093b6c99527527443006b7b77ac9e82d
areyeslo <areyeslo@users.noreply.github.com>
2024-09-06 12:33:50 -0600

    Adding FaWater icon to Media Source when sourceId is selected. (#2267)

--- app/subscriber/src/features/search-page/components/advanced-search/components/sections/MediaSection.tsx
+++ app/subscriber/src/features/search-page/components/advanced-search/components/sections/MediaSection.tsx
@@ -1,5 +1,6 @@
 import { useSubMediaGroups } from 'features/hooks';
 import React from 'react';
+import { FaWater } from 'react-icons/fa';
 import { IoIosArrowDropdownCircle, IoIosArrowDroprightCircle } from 'react-icons/io';
 import { useContent } from 'store/hooks';
 import {
@@ -34,7 +35,7 @@ export const MediaSection: React.FC<IMediaSectionProps> = ({
 }) => {
   const { subMediaGroups } = useSubMediaGroups(sources, series, mediaTypes);
   const [mediaGroupExpanded, setMediaGroupExpanded] = React.useState<ISubMediaGroupExpanded>({});
-
+  const [uniqueId] = React.useState(new Date().getTime().toString(36));
   const [
     {
       search: { filter },
@@ -44,116 +45,134 @@ export const MediaSection: React.FC<IMediaSectionProps> = ({
 
   return (
     <Col className="expanded media-section space-top">
-      {subMediaGroups?.map((mediaGroup, index) => (
-        <Col key={`${mediaGroup.key}-${index}`} className="sub-group">
-          <Row
-            className="sub-group-title"
-            onClick={() => {
-              setMediaGroupExpanded({
-                ...mediaGroupExpanded,
-                [mediaGroup.key]: !mediaGroupExpanded![mediaGroup.key],
-              });
-            }}
-          >
-            {`${mediaGroup.label} (${mediaGroup.options.length})`}
-            {!mediaGroupExpanded![mediaGroup.key] ? (
-              <IoIosArrowDroprightCircle
-                className="drop-icon"
-                onClick={() =>
-                  setMediaGroupExpanded({
-                    ...mediaGroupExpanded,
-                    [mediaGroup.key]: true,
-                  })
-                }
-              />
-            ) : (
-              <IoIosArrowDropdownCircle
-                className="drop-icon"
-                onClick={() => {
-                  setMediaGroupExpanded({
-                    ...mediaGroupExpanded,
-                    [mediaGroup.key]: false,
-                  });
-                }}
-              />
-            )}
-          </Row>
-          <Row className="sub-container" justifyContent="center">
-            <Show visible={!!mediaGroup.options.length && mediaGroupExpanded![mediaGroup.key]}>
-              <Show visible={!displayFiltersAsDropdown}>
-                <div className="check-box-list">
-                  <div className="chk-box-container chk-source select-all">
-                    <Button
-                      variant={ButtonVariant.link}
-                      onClick={() =>
-                        storeFilter({
-                          ...filter,
-                          sourceIds: sortableMediaOptions(mediaGroup.options).map((m) => +m.value!),
-                        })
-                      }
-                    >
-                      Select All
-                    </Button>
-                    /
-                    <Button
-                      variant={ButtonVariant.link}
-                      onClick={() => {
-                        let subsetSourceIds = sortableMediaOptions(mediaGroup.options).map(
-                          (s) => +s.value,
-                        );
-                        let filteredSourceIds = filter.sourceIds ?? [];
-                        subsetSourceIds.forEach(
-                          (s) => (filteredSourceIds = filteredSourceIds.filter((f) => f !== s)),
-                        );
-                        storeFilter({ ...filter, sourceIds: filteredSourceIds });
-                      }}
-                    >
-                      Deselect All
-                    </Button>
-                  </div>
-                  {sortableMediaOptions(mediaGroup.options).map((item, index) => (
-                    <div
-                      key={`chk-source-${index}-${item.value}`}
-                      className="chk-box-container chk-source"
-                    >
-                      <Checkbox
-                        id={`chk-source-${index}-${item.value}`}
-                        label={item.label}
-                        checked={filter.sourceIds?.includes(+item.value!)}
-                        value={item.value}
-                        onChange={(e) => {
-                          storeFilter({
-                            ...filter,
-                            sourceIds: e.target.checked
-                              ? [...(filter.sourceIds ?? []), +e.target.value] // add it
-                              : filter.sourceIds?.filter((i) => i !== +e.target.value), // remove it
-                          });
-                        }}
-                      />
-                    </div>
-                  ))}
-                </div>
-              </Show>
-              <Show visible={displayFiltersAsDropdown}>
-                <Select
-                  isMulti
-                  key={filter.sourceIds?.join(',')}
-                  defaultValue={determineSelectedMedia(filter, mediaGroup.options)}
-                  options={sortableMediaOptions(mediaGroup.options)}
-                  name="opts"
-                  onChange={(newValues) => {
-                    Array.isArray(newValues) &&
-                      storeFilter({
-                        ...filter,
-                        sourceIds: newValues.map((v) => v.value),
-                      });
+      {subMediaGroups?.map((mediaGroup, index) => {
+        // Check if any of the mediaGroup options is selected
+        const isMediaGroupFiltered = mediaGroup.options.some((option) =>
+          filter.sourceIds?.includes(option.id!),
+        );
+
+        return (
+          <Col key={`${mediaGroup.key}-${index}`} className="sub-group">
+            <Row
+              className="sub-group-title"
+              onClick={() => {
+                setMediaGroupExpanded({
+                  ...mediaGroupExpanded,
+                  [mediaGroup.key]: !mediaGroupExpanded![mediaGroup.key],
+                });
+              }}
+            >
+              {`${mediaGroup.label} (${mediaGroup.options.length})`}
+              {!mediaGroupExpanded![mediaGroup.key] ? (
+                <Col className="action-icons">
+                  {isMediaGroupFiltered && (
+                    <FaWater
+                      className="active-filter-icon"
+                      data-tooltip-id={`button-tip-${uniqueId}`}
+                      data-tooltip-content="Expand to see current filter settings"
+                    />
+                  )}
+                  <IoIosArrowDroprightCircle
+                    className="drop-icon"
+                    onClick={() =>
+                      setMediaGroupExpanded({
+                        ...mediaGroupExpanded,
+                        [mediaGroup.key]: true,
+                      })
+                    }
+                  />
+                </Col>
+              ) : (
+                <IoIosArrowDropdownCircle
+                  className="drop-icon"
+                  onClick={() => {
+                    setMediaGroupExpanded({
+                      ...mediaGroupExpanded,
+                      [mediaGroup.key]: false,
+                    });
                   }}
                 />
+              )}
+            </Row>
+            <Row className="sub-container" justifyContent="center">
+              <Show visible={!!mediaGroup.options.length && mediaGroupExpanded![mediaGroup.key]}>
+                <Show visible={!displayFiltersAsDropdown}>
+                  <div className="check-box-list">
+                    <div className="chk-box-container chk-source select-all">
+                      <Button
+                        variant={ButtonVariant.link}
+                        onClick={() =>
+                          storeFilter({
+                            ...filter,
+                            sourceIds: sortableMediaOptions(mediaGroup.options).map(
+                              (m) => +m.value!,
+                            ),
+                          })
+                        }
+                      >
+                        Select All
+                      </Button>
+                      /
+                      <Button
+                        variant={ButtonVariant.link}
+                        onClick={() => {
+                          let subsetSourceIds = sortableMediaOptions(mediaGroup.options).map(
+                            (s) => +s.value,
+                          );
+                          let filteredSourceIds = filter.sourceIds ?? [];
+                          subsetSourceIds.forEach(
+                            (s) => (filteredSourceIds = filteredSourceIds.filter((f) => f !== s)),
+                          );
+                          storeFilter({ ...filter, sourceIds: filteredSourceIds });
+                        }}
+                      >
+                        Deselect All
+                      </Button>
+                    </div>
+                    {sortableMediaOptions(mediaGroup.options).map((item, index) => (
+                      <div
+                        key={`chk-source-${index}-${item.value}`}
+                        className="chk-box-container chk-source"
+                      >
+                        <Checkbox
+                          id={`chk-source-${index}-${item.value}`}
+                          label={item.label}
+                          checked={filter.sourceIds?.includes(+item.value!)}
+                          value={item.value}
+                          onChange={(e) => {
+                            storeFilter({
+                              ...filter,
+                              sourceIds: e.target.checked
+                                ? [...(filter.sourceIds ?? []), +e.target.value] // add it
+                                : filter.sourceIds?.filter((i) => i !== +e.target.value), // remove it
+                            });
+                          }}
+                        />
+                      </div>
+                    ))}
+                  </div>
+                </Show>
+                <Show visible={displayFiltersAsDropdown}>
+                  <Select
+                    isMulti
+                    key={filter.sourceIds?.join(',')}
+                    defaultValue={determineSelectedMedia(filter, mediaGroup.options)}
+                    options={sortableMediaOptions(mediaGroup.options)}
+                    name="opts"
+                    onChange={(newValues) => {
+                      Array.isArray(newValues) &&
+                        storeFilter({
+                          ...filter,
+                          sourceIds: newValues.map((v) => v.value),
+                        });
+                    }}
+                  />
+                </Show>
               </Show>
-            </Show>
-          </Row>
-        </Col>
-      ))}
+            </Row>
+          </Col>
+        );
+      })}
     </Col>
   );
 };
