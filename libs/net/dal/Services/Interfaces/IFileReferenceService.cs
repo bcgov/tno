116573f3c128afb5865a2e3c9db412c4bb63f534
Jacob Wang <cpcw1cpcw@gmail.com>
2024-09-11 09:47:11 -0700

    default is 100

--- libs/net/dal/Services/Interfaces/IFileReferenceService.cs
+++ libs/net/dal/Services/Interfaces/IFileReferenceService.cs
@@ -18,7 +18,7 @@ public interface IFileReferenceService : IBaseService<FileReference, long>
 
     FileReference Attach(Content content, FileInfo file, string folderPath, bool deleteOriginal = true);
 
-    Task<IEnumerable<FileReference>> GetFiles(DateTime? updatedBefore = null, int limit = 3);
+    Task<IEnumerable<FileReference>> GetFiles(DateTime? updatedBefore = null, int limit = 100);
 
     Task<bool> UploadToS3Async(string s3Key, Stream fileStream);
 }
