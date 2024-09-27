2de0c47d7fffc44c28060d3b5c5e4b37cfa5fdd5
Jeremy Foster <jeremy.foster@fosol.ca>
2024-09-27 06:55:58 -0700

    Fix papers filter

--- app/editor/src/features/content/papers/constants/defaultPaperFilter.ts
+++ app/editor/src/features/content/papers/constants/defaultPaperFilter.ts
@@ -1,5 +1,6 @@
 import { IContentListFilter } from 'features/content/interfaces';
 import { defaultPage } from 'features/content/list-view/constants';
+import moment from 'moment';
 import { ContentTypeName, ISourceModel } from 'tno-core';
 
 import { defaultSort } from './defaultSort';
@@ -10,6 +11,11 @@ import { defaultSort } from './defaultSort';
  * @returns Paper filter.
  */
 export const defaultPaperFilter = (sources: ISourceModel[] = []): IContentListFilter => {
+  // Return stories that are also from yesterday if published after 10PM.
+  const yesterdayAt10PM = moment()
+    .subtract(1, 'days')
+    .set({ hour: 22, minute: 0, second: 0, millisecond: 0 });
+
   return {
     pageIndex: defaultPage.pageIndex,
     pageSize: defaultPage.pageSize,
@@ -23,7 +29,8 @@ export const defaultPaperFilter = (sources: ISourceModel[] = []): IContentListFi
     sourceIds: sources.map((s) => s.id),
     ownerId: 0,
     userId: '',
-    timeFrame: 0,
+    timeFrame: '',
+    startDate: yesterdayAt10PM.format(),
     commentary: false,
     topStory: false,
     featuredStory: false,
