b0bc765e68b7b5ae50ffb3448530030c25ebf0af
Jacob Wang <cpcw1cpcw@gmail.com>
2024-11-27 16:57:27 -0800

    set default

--- app/editor/src/features/admin/reports/constants/defaultReport.ts
+++ app/editor/src/features/admin/reports/constants/defaultReport.ts
@@ -32,6 +32,7 @@ export const defaultReport: IReportModel = {
       copyPriorInstance: false,
       clearOnStartNewReport: true,
       excludeContentInUnsentReport: false,
+      omitBCUpdates: false,
     },
     sections: {
       usePageBreaks: false,
