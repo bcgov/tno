f7fa660218d7cb42f701bd1b04bce50e44f0755e
Jeremy Foster <jeremy.foster@fosol.ca>
2025-01-14 15:52:40 -0800

    Fix db-connect script (#2381)

--- openshift/scripts/database-connect.sh
+++ openshift/scripts/database-connect.sh
@@ -9,7 +9,7 @@ if [ ! $1 ]; then
   die "First argument require to specify the environment [dev,test,prod,tools]."
 fi
 
-POD_NAME=$(oc get pods -n 9b301c-$1 --selector postgres-operator.crunchydata.com/role=master --no-headers -o custom-columns=POD:.metadata.name)
+POD_NAME=$(oc get pods -n 9b301c-$1 --selector statefulset.kubernetes.io/pod-name=postgres-0 --no-headers -o custom-columns=POD:.metadata.name)
 
 if [[ ! "$POD_NAME" ]]; then
  die "No pod found"
