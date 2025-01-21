8eedd6901113f62ac874761ef78bc1e413d94947
Jeremy Foster <jeremy.foster@fosol.ca>
2025-01-21 07:18:45 -0800

    Fix charts

--- api/node/src/utils/applyChartJsOptions.ts
+++ api/node/src/utils/applyChartJsOptions.ts
@@ -4,30 +4,31 @@ export const applyChartJsOptions = (values: any) => {
   // Ensure default configuration properties exist.
   if (options.plugins === undefined) {
     options.plugins = {};
-    if (options.plugins.datalabels === undefined) {
-      options.plugins.datalabels = {};
-      if (options.plugins.datalabels.labels === undefined) {
-        options.plugins.datalabels.labels = {};
-        if (options.plugins.datalabels.labels.title === undefined) {
-          options.plugins.datalabels.labels.title = {};
-        }
-      }
-    }
-    if (options.plugins.legend === undefined) {
-      options.plugins.legend = {};
-      if (options.plugins.legend.labels === undefined) {
-        options.plugins.legend.labels = {};
-      }
-      if (options.plugins.legend.title === undefined) {
-        options.plugins.legend.title = {};
-      }
-    }
-    if (options.plugins.title === undefined) {
-      options.plugins.title = {};
-    }
-    if (options.plugins.subtitle === undefined) {
-      options.plugins.subtitle = {};
-    }
+  }
+  if (options.plugins.datalabels === undefined) {
+    options.plugins.datalabels = {};
+  }
+  if (options.plugins.datalabels.labels === undefined) {
+    options.plugins.datalabels.labels = {};
+  }
+  if (options.plugins.datalabels.labels.title === undefined) {
+    options.plugins.datalabels.labels.title = {};
+  }
+
+  if (options.plugins.legend === undefined) {
+    options.plugins.legend = {};
+  }
+  if (options.plugins.legend.labels === undefined) {
+    options.plugins.legend.labels = {};
+  }
+  if (options.plugins.legend.title === undefined) {
+    options.plugins.legend.title = {};
+  }
+  if (options.plugins.title === undefined) {
+    options.plugins.title = {};
+  }
+  if (options.plugins.subtitle === undefined) {
+    options.plugins.subtitle = {};
   }
 
   if (options.plugins.datalabels.labels.title.display === undefined) {
