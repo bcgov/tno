06f209221b612b21885bf488c8076e359df35037
Dao Yu Lai <daoyu.lai@gov.bc.ca>
2025-01-27 12:53:37 -0800

    MMI-3103 Updated content commentary timeout filter (#2393)
    * MMI-3103 Updated content commentary timeout filter
    
    - updated content commentary timeout filter to be
    based on the commentary action updated on time

--- app/subscriber/src/features/commentary/utils/filterCommentaryResults.ts
+++ app/subscriber/src/features/commentary/utils/filterCommentaryResults.ts
@@ -16,9 +16,15 @@ export const filterCommentaryResults = (
   return results.filter((content) => {
     const postedCutoff = determineCommentaryTime(commentaryActionId, content.actions);
     const publishedCutoff = moment().subtract(1, 'month');
+    let commentaryUpdatedOn = null;
+    // use the commentary action updated on time to decide if the content should be shown in the commentary list
+    const commentaryAction = content.actions.find((a) => a.id === commentaryActionId);
+    if (commentaryAction) {
+      commentaryUpdatedOn = commentaryAction.updatedOn;
+    }
     return (
-      content.postedOn &&
-      moment(content.postedOn) >= moment(postedCutoff) &&
+      commentaryUpdatedOn &&
+      moment(commentaryUpdatedOn) >= moment(postedCutoff) &&
       moment(content.publishedOn) >= publishedCutoff
     );
   });
