8d6ebca7b7868dfbff061332b646d4b7ed039fe5
Jeremy Foster <jeremy.foster@fosol.ca>
2025-02-17 09:01:21 -0800

    Enhance Executive Summary

--- libs/net/dal/Configuration/ReportSectionConfiguration.cs
+++ libs/net/dal/Configuration/ReportSectionConfiguration.cs
@@ -4,11 +4,15 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using TNO.Entities;
 
-public class ReportSectionConfiguration : BaseTypeConfiguration<ReportSection, int>
+public class ReportSectionConfiguration : AuditColumnsConfiguration<ReportSection>
 {
     public override void Configure(EntityTypeBuilder<ReportSection> builder)
     {
         builder.Property(m => m.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Property(m => m.Name).IsRequired().HasMaxLength(100);
+        builder.Property(m => m.IsEnabled);
+        builder.Property(m => m.Description).IsRequired().HasColumnType("text").HasDefaultValueSql("''");
+        builder.Property(m => m.SortOrder).HasDefaultValue(0);
         builder.Property(m => m.ReportId).IsRequired();
         builder.Property(m => m.SectionType).IsRequired();
         builder.Property(m => m.FilterId);
@@ -22,6 +26,7 @@ public class ReportSectionConfiguration : BaseTypeConfiguration<ReportSection, i
         builder.HasOne(m => m.LinkedReport).WithMany().HasForeignKey(m => m.LinkedReportId).OnDelete(DeleteBehavior.Cascade);
         builder.HasMany(m => m.ChartTemplates).WithMany(m => m.ReportSections).UsingEntity<ReportSectionChartTemplate>();
 
+        builder.HasIndex(m => new { m.IsEnabled, m.Name }, $"IX_{typeof(ReportSection).Name.ToLower()}_is_enabled");
         builder.HasIndex(m => new { m.ReportId, m.Name }).IsUnique();
 
         base.Configure(builder);
---
+++ libs/net/dal/Migrations/20250217170030_1.3.21.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using TNO.DAL;
+
+#nullable disable
+
+namespace TNO.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class _1321 : SeedMigration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            PreUp(migrationBuilder);
+            migrationBuilder.AlterColumn<string>(
+                name: "description",
+                table: "report_section",
+                type: "text",
+                nullable: false,
+                defaultValueSql: "''",
+                oldClrType: typeof(string),
+                oldType: "character varying(2000)",
+                oldMaxLength: 2000,
+                oldDefaultValueSql: "''");
+            PostUp(migrationBuilder);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            PreDown(migrationBuilder);
+            migrationBuilder.AlterColumn<string>(
+                name: "description",
+                table: "report_section",
+                type: "character varying(2000)",
+                maxLength: 2000,
+                nullable: false,
+                defaultValueSql: "''",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldDefaultValueSql: "''");
+            PostDown(migrationBuilder);
+        }
+    }
+}
