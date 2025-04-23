6ab205aa33b4b28df79de2d282c3909d820e867b
nehalaggarwal-bcgov <nehal.aggarwal@gov.bc.ca>
2025-04-23 13:09:05 -0700

    MMI-3226 changes suggested on pull request

--- libs/net/dal/Services/Interfaces/IMediaAnalyticsService.cs
+++ libs/net/dal/Services/Interfaces/IMediaAnalyticsService.cs
@@ -7,9 +7,6 @@ namespace TNO.DAL.Services;
 public interface IMediaAnalyticsService : IBaseService<MediaAnalytics, int>
 {
     Task<IEnumerable<MediaAnalytics>> GetAllInformation();
-    Task<MediaAnalytics> FilterBySourceId(int sourceid);
-    Task<MediaAnalytics> FilterByMediaType(int mediatypeid);
-
     IPaged<MediaAnalytics> Find(MediaAnalyticsFilter filter);
 
 }
