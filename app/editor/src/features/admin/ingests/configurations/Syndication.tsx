4e503a1ea5fec00e90dcbb4216a078ac5dffb5c1
Jeremy Foster <jeremy.foster@fosol.ca>
2025-05-16 16:14:58 -0700

    MMI-3249 Update Syndication Service

--- app/editor/src/features/admin/ingests/configurations/Syndication.tsx
+++ app/editor/src/features/admin/ingests/configurations/Syndication.tsx
@@ -1,14 +1,66 @@
 import { useFormikContext } from 'formik';
 import React from 'react';
-import { FormikCheckbox, FormikSelect, FormikText, IIngestModel } from 'tno-core';
+import { FlexboxTable, FormikCheckbox, FormikSelect, FormikText, IIngestModel } from 'tno-core';
 
 import { TimeZones } from './constants';
+import { columns } from './constants/columns';
 import { ImportContent } from './ImportContent';
 import * as styled from './styled';
 
 export const Syndication: React.FC = (props) => {
   const { values, setFieldValue } = useFormikContext<IIngestModel>();
   const timeZone = TimeZones.find((t) => t.value === values.configuration.timeZone);
+
+  const initialItems = React.useCallback(() => {
+    const data: any[] = [];
+    let id = 0;
+    const keyValues = values.configuration.sources?.split('&') ?? [];
+    keyValues.forEach((x: string) => {
+      data.push({ id: ++id, name: x.split('=')[0], source: x.split('=')[1] });
+    });
+    data.push({ id: ++id, name: '', source: '' });
+    return data;
+  }, [values.configuration.sources]);
+
+  const [items, setItems] = React.useState<{ id: number; name: string; source: string }[]>(
+    initialItems(),
+  );
+
+  const updateItems = React.useCallback(
+    (updatedItems: any[]) => {
+      if (updatedItems.filter((x) => !x.name && !x.source).length === 0)
+        updatedItems.push({ id: updatedItems.length + 1, name: '', source: '' });
+      setItems(updatedItems);
+      setFieldValue(
+        'configuration.sources',
+        updatedItems
+          .filter((t) => !!t.name)
+          .map((item) => `${item.name?.trim()}=${item.source?.trim()}`)
+          .join('&'),
+      );
+    },
+    [setFieldValue],
+  );
+
+  const handleRemove = async (id: number) => {
+    const item = items.find((i) => i.id === id);
+    if (!item) return;
+
+    const updatedItems = items.filter((i) => i.id !== id);
+    updateItems(updatedItems);
+  };
+
+  const onChange = (event: any, cell: any, isSource = true) => {
+    const pointer = event.target.selectionStart;
+    window.requestAnimationFrame(() => {
+      event.target.selectionEnd = pointer;
+    });
+    const updatedItem = isSource
+      ? { ...cell.original, source: event.target.value }
+      : { ...cell.original, name: event.target.value };
+    const updatedItems = items.map((item) => (item.id === cell.original.id ? updatedItem : item));
+    updateItems(updatedItems);
+  };
 
   return (
     <styled.IngestType>
@@ -45,6 +97,15 @@ export const Syndication: React.FC = (props) => {
         onChange={(e) => {
           setFieldValue('configuration.fetchContent', e.currentTarget.checked);
         }}
+      />
+      <b>Source Mapping</b>
+      <FlexboxTable
+        rowId="id"
+        data={items}
+        columns={columns(handleRemove, onChange)}
+        showSort={true}
+        showActive={false}
+        pagingEnabled={false}
       />
     </styled.IngestType>
   );
