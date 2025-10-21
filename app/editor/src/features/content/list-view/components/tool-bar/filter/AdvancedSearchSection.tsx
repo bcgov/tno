a95c8cf0f31514084e5eb26408c6a12bcdd96cc9
AlessiaYChen <alessia.yanchen@gov.bc.ca>
2025-10-21 08:53:03 -0700

    use utility function to sort

--- app/editor/src/features/content/list-view/components/tool-bar/filter/AdvancedSearchSection.tsx
+++ app/editor/src/features/content/list-view/components/tool-bar/filter/AdvancedSearchSection.tsx
@@ -20,6 +20,7 @@ import {
   Select,
   SelectDate,
   Show,
+  sortObject,
   Text,
   ToolBarSection,
 } from 'tno-core';
@@ -44,11 +45,9 @@ export const AdvancedSearchSection: React.FC<IAdvancedSearchSectionProps> = () =
     const enabledOptions = seriesOptions.filter(
       (option) => option.value !== undefined && allowedSeries.has(Number(option.value)),
     );
-    const sortedOptions = [...enabledOptions].sort((a, b) => {
-      const labelA = typeof a.label === 'string' ? a.label : String(a.label ?? '');
-      const labelB = typeof b.label === 'string' ? b.label : String(b.label ?? '');
-      return labelA.localeCompare(labelB, undefined, { sensitivity: 'base' });
-    });
+    const sortedOptions = [...enabledOptions].sort(
+      sortObject((item) => String(item.label ?? '').toLocaleLowerCase()),
+    );
     return [new OptionItem('()', '()'), ...sortedOptions];
   }, [series, seriesOptions]);
 
--- app/editor/src/features/content/papers/components/AdvancedFilter.tsx
+++ app/editor/src/features/content/papers/components/AdvancedFilter.tsx
@@ -20,6 +20,7 @@ import {
   Select,
   SelectDate,
   Show,
+  sortObject,
   Text,
   ToolBarSection,
 } from 'tno-core';
@@ -54,11 +55,9 @@ export const AdvancedFilter: React.FC<IAdvancedFilterProps> = ({
     const enabledOptions = seriesOptions.filter(
       (option) => option.value !== undefined && allowedSeries.has(Number(option.value)),
     );
-    const sortedOptions = [...enabledOptions].sort((a, b) => {
-      const labelA = typeof a.label === 'string' ? a.label : String(a.label ?? '');
-      const labelB = typeof b.label === 'string' ? b.label : String(b.label ?? '');
-      return labelA.localeCompare(labelB, undefined, { sensitivity: 'base' });
-    });
+    const sortedOptions = [...enabledOptions].sort(
+      sortObject((item) => String(item.label ?? '').toLocaleLowerCase()),
+    );
     return [new OptionItem('()', '()'), ...sortedOptions];
   }, [series, seriesOptions]);
 
